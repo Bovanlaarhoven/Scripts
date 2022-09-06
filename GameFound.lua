78d620a958bb56644be1dd6fbe7285200bd879f8
Bo <bo.van.laarhoven@gmail.com>
2022-09-06 16:33:33 +0200

    .

--- GameFound.lua
+++
@@ -1,8 +0,0 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-
-OrionLib:MakeNotification({
-    Name = "Hydra Network",
-    Content = "Game Found!",
-    Image = "rbxassetid://4483345998",
-    Time = 4
-})
