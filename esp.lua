0beee2f9bf0fdfe6143c5617aacb7f1805f77a09
Bo <bo.van.laarhoven@gmail.com>
2022-09-08 20:24:47 +0200

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,45 +1,30 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-local players = game.Players:GetPlayers()
+local ESPColor = Color3.fromRGB(0, 255, 0) -- Your color
 
-local Tab = Window:MakeTab({
-    Name = "Tab 1",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-
-
-function esp(v)
-        repeat wait() until v.Character.Humanoid
-        if v ~= game.Players.LocalPlayer then
-        local esp = Instance.new("Highlight")
-        esp.Name = v.Name
-        esp.FillTransparency = 0.75
-        esp.FillColor = Color3.new(255, 0, 0)
-        esp.OutlineColor = Color3.new(0,0,0)
-        esp.OutlineTransparency = 0
-        esp.Parent = v.Character
+local function remove()
+    for i, v in pairs(game.Players:GetPlayers()) do
+        for i, v in pairs(v.Character:GetChildren()) do
+            if v:IsA("Highlight") then
+                v:Destroy()
+            end
         end
     end
-    
-    for i,v in pairs(players) do
-        esp(v)
-        v.CharacterAdded:Connect(function()
-            esp(v)
-        end)
+end
+
+    local function create()
+        local hl = Instance.new("Highlight")
+        hl.Parent = character
+        hl.FillColor = ESPColor
     end
     
-    game.Players.PlayerAdded:Connect(function(v)
-        v.CharacterAdded:Connect(function()
-            esp(v)
+    for i, v in pairs(game.Players:GetPlayers()) do
+        character = v.Character
+        create()
+    end
+    
+    game.Players.PlayerAdded:Connect(function(plr)
+        plr.CharacterAdded:Connect(function()
+            remove()
+            character = plr.Character
+            create()
         end)
     end)
-
-
-Tab:AddToggle({
-    Name = "Esp",
-    Default = false,
-    Callback = function(Value)
-        esp(v)
-    end    
-})
