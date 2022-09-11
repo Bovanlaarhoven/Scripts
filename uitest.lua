97b0995f3883a6652be86269fec163589664453f
Bo <bo.van.laarhoven@gmail.com>
2022-09-11 15:09:37 +0200

    Create uitest.lua

---
+++ uitest.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
