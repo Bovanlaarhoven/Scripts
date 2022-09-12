b0950b294f077372ed2b5bbac298f99aae21db4f
Bo <bo.van.laarhoven@gmail.com>
2022-09-12 18:13:51 +0200

    Delete uitest.lua

--- uitest.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
-local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
