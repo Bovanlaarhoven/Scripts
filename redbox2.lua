fbcfd9426abf2e5bef97aee74b24bd89a5b6b1d1
Bo <bo.van.laarhoven@gmail.com>
2022-09-19 17:23:31 +0200

    redbox

--- redbox2.lua
+++ redbox2.lua
@@ -1,5 +1,15 @@
+local StarterPlayer = game:GetService("StarterPlayer")
+local Workspace = game:GetService("Workspace")
+local Light = game:GetService("Lighting")
+local players = game.Players:GetPlayers()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Redbox 2", HidePremium = false, IntroText = "Hydra Network", SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local MainTab = Window:MakeTab({
+        Name = "Main",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
 
 getgenv().HeadSize = 10
 getgenv().Disabled = true
@@ -8,22 +18,26 @@ getgenv().Disabled = true
         for i,v in next, game:GetService('Players'):GetPlayers() do
         if v.Name ~= game:GetService('Players').LocalPlayer.Name then
         pcall(function()
-        v.Character.HumanoidRootPart.Size = Vector3.new(HeadSize,HeadSize,HeadSize)
-        v.Character.HumanoidRootPart.Transparency = 0.7
-        v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really blue")
-        v.Character.HumanoidRootPart.Material = "ForceField"
-        v.Character.HumanoidRootPart.CanCollide = false
+        v.Character.Head.Size = Vector3.new(HeadSize,HeadSize,HeadSize)
+        v.Character.Head.Transparency = 0.7
+        MainTab:AddColorpicker({
+                Name = "Colorpicker",
+                Default = Color3.fromRGB(255, 0, 0),
+                Callback = function(Value)
+                        v.Character.Head.BrickColor = Value
+                end	  
+        })
+        
+        v.Character.Head.Material = "ForceField"
+        v.Character.Head.CanCollide = false
         end)
         end
         end
         end
         end)
 
-local MainTab = Window:MakeTab({
-    Name = "Main",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+
+
 
 local PlayerTab = Window:MakeTab({
 	Name = "Local Player",
@@ -53,6 +67,18 @@ MainTab:AddButton({
   	end    
 })
 
+PlayerTab:AddButton({
+	Name = "Full Bright.",
+	Callback = function()
+        Light.Ambient = Color3.new(1, 1, 1)
+        Light.ColorShift_Bottom = Color3.new(1, 1, 1)
+        Light.ColorShift_Top = Color3.new(1, 1, 1)
+        game.Lighting.FogEnd = 100000
+        game.Lighting.FogStart = 0
+        game.Lighting.Brightness = 5
+        game.Lighting.GlobalShadows = false
+  	end    
+})
 MainTab:AddSlider({
     Name = "Hitbox",
     Min = 0,
@@ -66,8 +92,9 @@ MainTab:AddSlider({
     end    
 })
 
+
 PlayerTab:AddSlider({
-	Name = "Fov Slider",
+        Name = "Fov Slider",
 	Min = 0,
 	Max = 120,
 	Default = 70,
@@ -75,21 +102,42 @@ PlayerTab:AddSlider({
 	Increment = 1,
 	ValueName = "Fov",
 	Callback = function(Value)
-		local amount = Value
-        game:GetService'Workspace'.Camera.FieldOfView = amount
+	        local amount = Value
+                game:GetService'Workspace'.Camera.FieldOfView = amount
 	end    
 })
 
 
+local TargetWalkspeed
 PlayerTab:AddSlider({
-	Name = "Speed Slider",
+	Name = "Speed (MoveDirection)",
+	Min = 0,
+	Max = 500,
+	Default = 0,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	Callback = function(Value)
+		TargetWalkspeed = Value
+	end   
+})
+
+PlayerTab:AddSlider({
+	Name = "Jump Power Slider",
 	Min = 0,
 	Max = 120,
 	Default = 16,
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
-	ValueName = "Fov",
+	ValueName = "Jump Power",
 	Callback = function(Value)
-        
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
 	end    
 })
+
+game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            if game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
+                game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * TargetWalkspeed/100)
+            end
+        end)
+    end)
