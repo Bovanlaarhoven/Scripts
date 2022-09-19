9674dfb0f2df87dbab2549e2fe40ecc311637497
Bo <44844728+Robobo2022@users.noreply.github.com>
2022-09-19 10:01:12 -0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,77 +1,59 @@
-local StarterPlayer = game:GetService("StarterPlayer")
-local Workspace = game:GetService("Workspace")
-local Light = game:GetService("Lighting")
-local players = game.Players:GetPlayers()
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Redbox 2", HidePremium = false, IntroText = "Hydra Network", SaveConfig = true, ConfigFolder = "OrionTest"})
+--discord.gg/boronide, code generated using luamin.js™
 
-getgenv().Color = BrickColor
-getgenv().HeadSize = 10
-getgenv().Disabled = true
-        game:GetService('RunService').RenderStepped:connect(function()
+
+
+local a= game:GetService("StarterPlayer")local a= game:GetService("Workspace")local a= game:GetService("Lighting")local b= game.Players:GetPlayers()local b= loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()local b= b:MakeWindow({Name = "Redbox 2", HidePremium = false, IntroText = "Hydra Network", SaveConfig = true, ConfigFolder = "OrionTest"})getgenv().Color=BrickColor;getgenv().HeadSize=10;getgenv().Disabled=true;game:GetService('RunService').RenderStepped:connect(function()
         if Disabled then
-        for i,v in next, game:GetService('Players'):GetPlayers() do
-        if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+        for a,a in next, game:GetService('Players'):GetPlayers() do
+        if a.Name ~= game:GetService('Players').LocalPlayer.Name then
         pcall(function()
-        v.Character.Head.Size = Vector3.new(HeadSize,HeadSize,HeadSize)
-        v.Character.Head.Transparency = 0.7
-        v.Character.Head.BrickColor = BrickColor.new(Color)
-        v.Character.Head.Material = "ForceField"
-        v.Character.Head.CanCollide = false
+        a.Character.Head.Size = Vector3.new(HeadSize,HeadSize,HeadSize)
+        a.Character.Head.Transparency = 0.7
+        a.Character.Head.BrickColor = BrickColor.new(Color)
+        a.Character.Head.Material = "ForceField"
+        a.Character.Head.CanCollide = false
         end)
         end
         end
         end
-        end)
-
-        
-local MainTab = Window:MakeTab({
+        end)local c= b:MakeTab({
     Name = "Main",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
-})
-
-local PlayerTab = Window:MakeTab({
+})local b= b:MakeTab({
 	Name = "Local Player",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
-})
-
-MainTab:AddBind({
+})c:AddBind({
 	Name = "Inf Ammo (equipe Weapon before pressing)",
 	Default = Enum.KeyCode.L,
 	Hold = false,
 	Callback = function()
-        local Tool = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool")
-        local m = require(Tool.toolSettings)
+        local a = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool")
+        local b = require(a.toolSettings)
         game:GetService("Players").LocalPlayer.Character.Humanoid:UnequipTools()
-        game:GetService("Players").LocalPlayer.Character.Humanoid:EquipTool(Tool)
-        game:GetService("ReplicatedStorage").Assets.Remotes.syncAmmo:FireServer(1000000,1000000, Tool)
+        game:GetService("Players").LocalPlayer.Character.Humanoid:EquipTool(a)
+        game:GetService("ReplicatedStorage").Assets.Remotes.syncAmmo:FireServer(1000000,1000000, a)
 	end    
-})
-
-MainTab:AddButton({
+})c:AddButton({
 	Name = "No Recoil",
 	Callback = function()	
-        local Tool = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool")
-        local m = require(Tool.toolSettings)
-        m.firingRecoilEnabled = false
+        local a = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool")
+        local a = require(a.toolSettings)
+        a.firingRecoilEnabled = false
   	end    
-})
-
-PlayerTab:AddButton({
+})b:AddButton({
 	Name = "Full Bright.",
 	Callback = function()
-        Light.Ambient = Color3.new(1, 1, 1)
-        Light.ColorShift_Bottom = Color3.new(1, 1, 1)
-        Light.ColorShift_Top = Color3.new(1, 1, 1)
+        a.Ambient = Color3.new(1, 1, 1)
+        a.ColorShift_Bottom = Color3.new(1, 1, 1)
+        a.ColorShift_Top = Color3.new(1, 1, 1)
         game.Lighting.FogEnd = 100000
         game.Lighting.FogStart = 0
         game.Lighting.Brightness = 5
         game.Lighting.GlobalShadows = false
   	end    
-})
-MainTab:AddSlider({
+})c:AddSlider({
     Name = "Hitbox",
     Min = 0,
     Max = 5,
@@ -79,20 +61,16 @@ MainTab:AddSlider({
     Color = Color3.fromRGB(255,255,255),
     Increment = 1,
     ValueName = "Head size",
-    Callback = function(Value)
-        HeadSize = Value
+    Callback = function(a)
+        HeadSize = a
     end    
-})
-
-MainTab:AddColorpicker({
+})c:AddColorpicker({
 	Name = "Hitbox Color",
 	Default = Color3.fromRGB(255, 0, 0),
-	Callback = function(Value)
-		Color = Value
+	Callback = function(a)
+		Color = a
 	end	  
-})
-
-PlayerTab:AddSlider({
+})b:AddSlider({
         Name = "Fov Slider",
 	Min = 0,
 	Max = 120,
@@ -100,27 +78,21 @@ PlayerTab:AddSlider({
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
 	ValueName = "Fov",
-	Callback = function(Value)
-	        local amount = Value
-                game:GetService'Workspace'.Camera.FieldOfView = amount
+	Callback = function(a)
+	        local a = a
+                game:GetService'Workspace'.Camera.FieldOfView = a
 	end    
-})
-
-
-local TargetWalkspeed
-PlayerTab:AddSlider({
+})local a;b:AddSlider({
 	Name = "Speed (MoveDirection)",
 	Min = 0,
 	Max = 500,
 	Default = 0,
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
-	Callback = function(Value)
-		TargetWalkspeed = Value
+	Callback = function(b)
+		a = b
 	end   
-})
-
-PlayerTab:AddSlider({
+})b:AddSlider({
 	Name = "Jump Power Slider",
 	Min = 0,
 	Max = 120,
@@ -128,15 +100,13 @@ PlayerTab:AddSlider({
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
 	ValueName = "Jump Power",
-	Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+	Callback = function(a)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = a
 	end    
-})
-
-game:GetService("RunService").RenderStepped:Connect(function()
+})game:GetService("RunService").RenderStepped:Connect(function()
         pcall(function()
             if game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
-                game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * TargetWalkspeed/100)
+                game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * a/100)
             end
         end)
     end)
