1afe852f3c21db8a7ab207864ab3fbc15a789de5
Bo <bo.van.laarhoven@gmail.com>
2022-09-21 21:41:11 +0200

    Big progress

--- Evade Rewritten.lua
+++ Evade Rewritten.lua
@@ -1,13 +1,15 @@
+game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
 local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
-
 local WorkspacePlayers = game:GetService("Workspace").Game.Players
 local Players = game:GetService('Players')
 local localplayer = Players.LocalPlayer
 local GuiService = game:GetService("GuiService")
 local Light = game:GetService("Lighting")
+Library:SetWatermarkVisibility(true)
+Library:SetWatermark('Hydra Network')
 
 getgenv().money = true
 getgenv().revivedie = true
@@ -25,8 +27,6 @@ getgenv().Settings = {
     Speed = 1450,
     Jump = 3,
     reviveTime = 3,
-    DownedColor = Color3.fromRGB(255,0,0),
-    PlayerColor = Color3.fromRGB(255,170,0),
 }
 
 local FindAI = function()
@@ -144,20 +144,123 @@ local Window = Library:CreateWindow({
     AutoShow = true,
 })
 
-
 local MainTab = {
     Main = Window:AddTab('Main Features'), 
-    ['UI Settings'] = Window:AddTab('UI Settings'),
 }
 
 local AutoFarms = MainTab.Main:AddLeftGroupbox('Auto Farms')
+local Modifiers = MainTab.Main:AddLeftGroupbox('Modifiers')
+local Keybinds = MainTab.Main:AddLeftGroupbox('Keybinds')
+local Buttons = MainTab.Main:AddRightGroupbox('Buttons')
 
+--tabs
 AutoFarms:AddToggle('MoneyFarm', {
     Text = 'Money Farm',
     Default = false,
     Tooltip = 'Auto Revives Players',
 })
 
-AutoFarms.MoneyFarm:OnChanged(function(Value)
+AutoFarms:AddToggle('AfkFarm', {
+    Text = 'Afk Farm',
+    Default = false,
+    Tooltip = 'Afks outside the map',
+})
+
+--toggles
+Toggles.MoneyFarm:OnChanged(function(Value)
     Settings.moneyfarm = Value
 end)
+
+Toggles.AfkFarm:OnChanged(function(Value)
+    Settings.afkfarm = Value
+end)
+
+--sliders
+local TargetWalkspeed
+Modifiers:AddSlider('Speed', {
+    Text = 'Speed slider',
+    Default = 0,
+    Min = 0,
+    Max = 250,
+    Rounding = 1,
+    Compact = false,
+})
+
+Options.Speed:OnChanged(function()
+    TargetWalkspeed = Options.Speed.Value
+end)
+
+Modifiers:AddSlider('jump', {
+    Text = 'Jump slider',
+    Default = 3,
+    Min = 0,
+    Max = 120,
+    Rounding = 1,
+    Compact = false,
+})
+Options.jump:OnChanged(function()
+    Settings.Jump = Options.jump.Value
+end)
+
+Modifiers:AddSlider('Time', {
+    Text = 'Time slider',
+    Default = 14,
+    Min = 0,
+    Max = 24,
+    Rounding = 1,
+    Compact = false,
+})
+Options.Time:OnChanged(function()
+    game.Lighting.ClockTime = Options.Time.Value
+end)
+
+--keybinds
+
+Keybinds:AddLabel('Respawn'):AddKeyPicker('Respawn', {
+    Default = 'R',
+    SyncToggleState = false, 
+    Mode = 'Hold',
+    Text = 'Respawn Keybind',
+    NoUI = false,
+})
+
+Options.Respawn:OnClick(function()
+     Respawn:GetState()
+end)
+
+task.spawn(function()
+    while true do
+        wait(1)
+        local state = Options.Respawn:GetState()
+        if state then
+            game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
+        end
+        if Library.Unloaded then break end
+    end
+end)
+
+--buttons
+
+local MyButton = Buttons:AddButton('Test Emote Giver', function()
+    game:GetService("ReplicatedStorage").Events.UI.Purchase:InvokeServer("Emotes", "Test")
+end)
+
+--others
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    pcall(function()
+        if game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
+            game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * TargetWalkspeed/250)
+        end
+    end)
+end)
+
+local old
+old = hookmetamethod(game,"__namecall",newcclosure(function(self,...)
+    local Args = {...}
+    local method = getnamecallmethod()
+    if tostring(self) == 'Communicator' and method == "InvokeServer" and Args[1] == "update" then
+        return Settings.Speed, Settings.Jump 
+    end
+    return old(self,...)
+end))
