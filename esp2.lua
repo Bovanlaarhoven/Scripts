39133358050cd83d10c5eb3b7c6d62fedbbff722
Bo <bo.van.laarhoven@gmail.com>
2022-10-01 22:07:32 +0200

    O:

---
+++ esp2.lua
@@ -0,0 +1,43 @@
+local Players = game:GetService("Players"):GetChildren()
+local RunService = game:GetService("RunService")
+local highlight = Instance.new("Highlight")
+highlight.Name = "Highlight"
+
+for i,v in pairs(Players) do
+    repeat wait() until v.Character
+    if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
+    local highlightclone = highlight:Clone()
+    highlightclone.Adornee = v.Character
+    highlightclone.Parent = v.Character:findFirstChild("HumanoidRootPart")
+    highlightclone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+    highlightclone.Name = "highlight"
+end
+end
+
+game.Players.PlayerAdded:Connect(function(player)
+    repeat wait() until player.Character
+    if not player.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
+    local highlightclone = highlight:Clone()
+    highlightclone.Adornee = player.Character
+    highlightclone.Parent = player.Character:findFirstChild("HumanoidRootPart")
+    highlightclone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+    highlightclone.Name = "highlight"
+    end
+end)
+
+game.Players.PlayerRemoving:Connect(function(playerRemoved)
+    playerRemoved.Character:FindFirstChild("HumanoidRootPart").Highlight:destroy()
+end)
+
+RunService.Heartbeat:Connect(function()
+    for i,v in pairs(Players) do
+        repeat wait() until v.Character
+        if not v.Character:FindFirstChild("HumoidRootPart").:FindFirstChild("Highlight") then
+        local highlightclone = highlight:Clone()
+        highlightclone.Adornee = v.Character
+        highlightclone.Parent = v.Character:findFirstChild("HumanoidRootPart")
+        highlightclone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+        highlightclone.Name = "highlight"
+    end
+end
+end)
