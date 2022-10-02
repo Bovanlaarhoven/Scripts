b999dc04e65f5ae17f0fc105c8ba83c88afdb169
Bo <bo.van.laarhoven@gmail.com>
2022-10-02 22:35:32 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,39 +1,92 @@
-local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
-local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
-local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
-local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
-local dwEntities = game:GetService("Players")
-local dwLocalPlayer = dwEntities.LocalPlayer 
-local dwRunService = game:GetService("RunService")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/cat"))()
+local Window = Library:CreateWindow("Speed Run 4 | PeachHub.lua", Vector2.new(492, 598), Enum.KeyCode.RightControl)
+local AimingTab = Window:CreateTab("Main")
 
-local Window = Library:CreateWindow({
-    Title = 'Esp',
-    Center = true, 
-    AutoShow = true,
-})
+getgenv().getallvls = true
+getgenv().getstars = true
 
-local Tabs = {
-    Main = Window:AddTab('Main'), 
-    ['UI Settings'] = Window:AddTab('UI Settings'),
-}
 
-local LeftGroupBox = Tabs.Main:AddLeftGroupbox('Groupbox')
+function GetAllStars()
+while getstars == true do task.wait()
+    local args = {
+        [1] = "Level 1"
+    } 
+    game:GetService("ReplicatedStorage").GotStar:FireServer(unpack(args))   
+end
+end
 
-local settings_tbl = {
-    ESP_Enabled = true,
-    ESP_TeamCheck = false,
-    Chams = true,
-    Chams_Color = Color3.fromRGB(0,0,255),
-    Chams_Transparency = 0.1,
-    Chams_Glow_Color = Color3.fromRGB(255,0,0)
-}
+function GetAllLvls()
+while getallvls == true do task.wait()
+    local args = {
+        [1] = 1660243517.868455,
+        [2] = "Level 1",
+        [3] = "Normal",
+        [4] = false
+    }
+    game:GetService("ReplicatedStorage").BeatLevel:FireServer(unpack(args))
+    wait(0.5)        
+end
+end
 
-LeftGroupBox:AddToggle('EspToggle', {
-    Text = 'Esp',
-    Default = false,
-    Tooltip = 'Turns on esp',
-})
+local otherSection = AimingTab:CreateSector("lvl switch", "right")
+local testSection = AimingTab:CreateSector("Main", "left")
+local textSection = AimingTab:CreateSector("(Switch Dimension When lvl 30)", "left")
 
-Toggles.EspToggle:OnChanged(function()
-    ESP_Enabled = true
+testSection:AddToggle("Give Stars", false, function(first)
+    getstars = first
+    GetAllStars()
 end)
+
+testSection:AddToggle("Auto lvl", false, function(first)
+    getallvls = first
+    GetAllLvls()
+end)
+
+otherSection:AddButton("Normal", function(IhateGayPeople)
+    local args = {
+        [1] = "Normal"
+    }  
+    game:GetService("ReplicatedStorage").SetDimension:FireServer(unpack(args))  
+end)
+
+otherSection:AddButton("Peach Desert", function(IhateGayPeople)
+    local args = {
+        [1] = "Peach Desert"
+    }
+    game:GetService("ReplicatedStorage").SetDimension:FireServer(unpack(args))
+end)
+
+otherSection:AddButton("Cybernaut", function(IhateGayPeople)
+    local args = {
+        [1] = "Cybernaut"
+    }
+    game:GetService("ReplicatedStorage").SetDimension:FireServer(unpack(args))
+end)
+
+otherSection:AddButton("Cosmic", function(IhateGayPeople)
+    local args = {
+        [1] = "Cosmic"
+    }
+    game:GetService("ReplicatedStorage").SetDimension:FireServer(unpack(args))
+end)
+
+otherSection:AddButton("Moon", function(IhateGayPeople)
+    local args = {
+        [1] = "Moon"
+    }
+    game:GetService("ReplicatedStorage").SetDimension:FireServer(unpack(args))
+end)
+
+otherSection:AddButton("Chromium", function(IhateGayPeople)
+    local args = {
+        [1] = "Chromium"
+    }
+    game:GetService("ReplicatedStorage").SetDimension:FireServer(unpack(args))
+end)
+
+otherSection:AddButton("Birchock", function(IhateGayPeople)
+    local args = {
+        [1] = "Birchock"
+    }
+    game:GetService("ReplicatedStorage").SetDimension:FireServer(unpack(args))
+end) 
