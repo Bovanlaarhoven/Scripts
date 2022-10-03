4ca707576aa4f9b0789a0f17e48696fcef0a0495
Bo <bo.van.laarhoven@gmail.com>
2022-10-03 22:22:13 +0200

    Update esp orion.lua

--- esp orion.lua
+++ esp orion.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Esp Gui", HidePremium = false, IntroText = "Noob gui", SaveConfig = true, ConfigFolder = "OrionTest"})
 local dwEntities = game:GetService("Players")
 local dwLocalPlayer = dwEntities.LocalPlayer 
 local dwRunService = game:GetService("RunService")
