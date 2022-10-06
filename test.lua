295e9d00046825a8d03630216b16b8df95dd4930
Bo <bo.van.laarhoven@gmail.com>
2022-10-06 22:42:04 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,31 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Players = game:GetService("Players"):GetChildren()
 
 local MainTab = Window:MakeTab({
 	Name = "Main",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+
+local PlayerTab = Window:MakeTab({
+	Name = "Player",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+game.Players.PlayerRemoving:Connect(function(playerRemoved)
+    playerRemoved.Character:FindFirstChild("HumanoidRootPart").Highlight:destroy()
+end)
+
+game.Players.PlayerAdded:Connect(function(player)
+    repeat wait() until player.Character
+    if not player.Character:FindFirstChild("HumanoidRootPart") then
+		PlayerTab:AddButton({
+			Name = "Button!",
+			Callback = function()
+					  print("button pressed")
+			  end    
+		})
+    end
+end)
