6e88bfe53885844c1f8a60448b519c7df02a8a6d
Bo <bo.van.laarhoven@gmail.com>
2022-10-17 22:52:53 +0200

    update

--- better roblox.lua
+++ better roblox.lua
@@ -1,31 +1,23 @@
-repeat wait() until game:IsLoaded() wait()
+repeat wait() until game:IsLoaded() wait(1)
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
-local notif1 = Notification.new("success", "Executor Type", identifyexecutor().."")
-notif1:deleteTimeout(2.5)
-wait(3)
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Better Roblox", HidePremium = true, IntroText = "Better Roblox", IntroIcon = "rbxassetid://7539983773", SaveConfig = true, ConfigFolder = "OrionTest"})
 local ownerId = game.CreatorId
-local player = game.Players.LocalPlayer
 
-local Tab = Window:MakeTab({
-	Name = "Main",
-	Icon = "rbxassetid://7539983773",
-	PremiumOnly = false
-})
+local notif1 = Notification.new("success", "Executor Type", identifyexecutor().."")
+notif1:deleteTimeout(3)
 
-OrionLib:MakeNotification({
-	Name = "Better Roblox",
-	Content =  player.Name.." Welcome" ,
-	Image = "rbxassetid://10993876184",
-	Time = 5
-})
+game.Players.PlayerAdded:Connect(function(player)
+    local notifi = Notification.new("success", "Player Joined", player.Name.." has joined")
+    notifi:deleteTimeout(1.5)
+end)
 
-Tab:AddToggle({
-	Name = "Player join",
-	Default = false,
-	Callback = function(Value)
-        Playerjoins = Value
-        playerjoin()
-	end    
-})
+game.Players.PlayerRemoving:Connect(function(player)
+    local notif = Notification.new("warning", "Player Left", player.Name.." has left")
+    notif:deleteTimeout(1.5)
+end)
+
+game.Players.PlayerAdded:Connect(function(player)
+    if player.UserId == ownerId then
+        game.Players.LocalPlayer:Kick("Owner Joined")
+    end
+end)
+
