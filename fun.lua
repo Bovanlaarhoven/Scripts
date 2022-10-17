08bec3564a05b2dba7afc3dc0a14096692156f56
Bo <bo.van.laarhoven@gmail.com>
2022-10-17 20:18:02 +0200

    Update fun.lua

--- fun.lua
+++ fun.lua
@@ -2,7 +2,7 @@ repeat wait() until game:IsLoaded() wait(1)
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
 local ownerId = game.CreatorId
 
-local notif = Notification.new("success", "Executor Type", identifyexecutor.." .")
+local notif = Notification.new("success", "Executor Type", identifyexecutor()..)
 notif:deleteTimeout(3)
 
 game.Players.PlayerAdded:Connect(function(player)
@@ -17,7 +17,7 @@ end)
 
 game.Players.PlayerAdded:Connect(function(player)
     if player.UserId == ownerId then
-        local notif = Notification.new("message", "Owner Joined", player.Name.." has joined")
+        game.Players.LocalPlayer:Kick("Owner Joined")
     end
 end)
 
