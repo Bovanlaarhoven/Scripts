b3eee1c2f11981f7d3adbe1a693e214f03e9abc5
Bo <bo.van.laarhoven@gmail.com>
2022-10-19 16:54:46 +0200

    Update test.lua

---
+++ test.lua
@@ -0,0 +1,35 @@
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Test", HidePremium = false, SaveConfig = true, IntroEnabled = false, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+	Name = "Joined",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Tab1 = Window:MakeTab({
+	Name = "Left",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+game.Players.PlayerAdded:Connect(function(player)
+    Tab:AddButton({
+        Name = player.Name.." Joined",
+        Callback = function()
+            local notif = Notification.new("success", "Player joined", player.Name.." joined")
+            notif:deleteTimeout(3)
+          end    
+    })    
+end)
+
+game.Players.PlayerRemoving:Connect(function(player)
+    Tab1:AddButton({
+        Name = player.Name.." Left",
+        Callback = function()
+            local notif = Notification.new("error", "Player Left", player.Name.." left")
+            notif:deleteTimeout(3)
+          end    
+    })    
+end)
