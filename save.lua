2a46be9f51d33840869747e2c3c8e4b684ef0f17
Bo <bo.van.laarhoven@gmail.com>
2022-10-24 08:13:26 +0200

    Update save.lua

--- save.lua
+++ save.lua
@@ -1,3 +1,6 @@
-local esp = Instance.new("Highlight")
+while true do
+    repeat wait() until game.Workspace.Game.Effects.Tickets.Ticket1
+    local esp = Instance.new("Highlight")
+    esp.Parent = game.Workspace.Game.Effects.Tickets.Ticket1
+end
 
-esp.Parent = players
