92747dc7cbeea95ae315ff6a9031d545a508fe73
Bo <bo.van.laarhoven@gmail.com>
2022-10-24 14:57:00 +0200

    Update test3.lua

--- test3.lua
+++ test3.lua
@@ -1,1 +1,8 @@
-local ticket = 
+for _,plrs in pairs(game.Players:GetPlayers()) do
+	if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - plrs.Character.HumanoidRootPart.Position).magnitude <= 20 then
+        local esp = Instance.new("Highlight")
+        esp.Parent = 
+    elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - plrs.Character.HumanoidRootPart.Position).magnitude > 20 then
+        print("player not near")
+	end
+end
