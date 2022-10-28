944b68469c1dd1039560e676fdb6f15c3804bb68
Bo <bo.van.laarhoven@gmail.com>
2022-10-28 22:16:03 +0200

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -590,13 +590,13 @@ MiscTab:AddButton({
 })
 
 MiscTab:AddButton({
-	Name = "Q to Teleport",
+	Name = "V to Teleport",
 	Callback = function()
         plr = game.Players.LocalPlayer 
         hum = plr.Character.HumanoidRootPart 
         mouse = plr:GetMouse()
         mouse.KeyDown:connect(function(key)
-            if key == "q" then
+            if key == "v" then
             if mouse.Target then
                 hum.CFrame = CFrame.new(mouse.Hit.x, mouse.Hit.y + 5, mouse.Hit.z)
                 end
