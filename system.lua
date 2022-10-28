dcc0c758e6dfa4d1108eb1c8dd87149a42be0869
Bo <bo.van.laarhoven@gmail.com>
2022-10-28 21:31:47 +0200

    Update system.lua

--- system.lua
+++ system.lua
@@ -1,7 +1,7 @@
 local GuiService = game:GetService("GuiService")
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Hydra network Keysystem", HidePremium = false, IntroText = "Key System", SaveConfig = false, ConfigFolder = "OrionTest"})
-getgenv().Key = "sdflkjsdlfkjsdlo"
+getgenv().Key = "ksjdhflksdjhflaksj"
 getgenv().Keyinput = "string"
 
 function MakeScript()
