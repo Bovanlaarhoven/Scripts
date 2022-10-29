182bb2e0f964005c4e0e38ad5cdb30f0bf7e705a
Bo <bo.van.laarhoven@gmail.com>
2022-10-29 11:26:57 +0200

    Update the wild west.lua

--- the wild west.lua
+++ the wild west.lua
@@ -18,8 +18,8 @@ local Window = Rayfield:CreateWindow({
 	}
 })
 
-local T1 = Window:CreateTab("Animal ESP")
-local T2 = Window:CreateTab("Ore ESP")
+local T1 = Window:CreateTab("Animal ESP(billboard)")
+local T2 = Window:CreateTab("Animal ESP(highlight)")
 
 local Button = T1:CreateButton({
 	Name = "Bear Esp",
@@ -126,26 +126,48 @@ local Button = T1:CreateButton({
 })
 
 local Button = T2:CreateButton({
-	Name = "Coal Esp",
+	Name = "Bear Esp",
 	Callback = function()
         for i,v in pairs(game.Workspace:GetDescendants()) do
-            if v.ClassName == "Model" and v.Parent.Name == "1" or v.Parent.Name == "2" or v.Parent.Name == "3" or v.Parent.Name == "4" then
-            local BillboardGui = Instance.new("BillboardGui")
-            local TextLabel = Instance.new("TextLabel")
-            
+            if v.ClassName == "Model" and v.Parent.Name == "Bear" then
+                local BillboardGui = Instance.new("Highlight")
+                BillboardGui.Parent = v.Parent
+            end
+        end
+	end,
+})
+
+local Button = T2:CreateButton({
+	Name = "Bison Esp",
+	Callback = function()
+        for i,v in pairs(game.Workspace:GetDescendants()) do
+            if v.ClassName == "Model" and v.Parent.Name == "Bison" then
+            local BillboardGui = Instance.new("Highlight")
             BillboardGui.Parent = v.Parent
-            BillboardGui.AlwaysOnTop = true
-            BillboardGui.LightInfluence = 1
-            BillboardGui.Size = UDim2.new(0, 50, 0, 50)
-            BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
-            
-            TextLabel.Parent = BillboardGui
-            TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
-            TextLabel.BackgroundTransparency = 1
-            TextLabel.Size = UDim2.new(1, 0, 1, 0)
-            TextLabel.Text = "Coal"
-            TextLabel.TextColor3 = Color3.new(0, 0, 0)
-            TextLabel.TextScaled = true
+            end
+        end
+	end,
+})
+
+local Button = T2:CreateButton({
+	Name = "Deer Esp",
+	Callback = function()
+        for i,v in pairs(game.Workspace:GetDescendants()) do
+            if v.ClassName == "Model" and v.Parent.Name == "Deer" then
+            local BillboardGui = Instance.new("Highlight")
+            BillboardGui.Parent = v.Parent
+            end
+        end
+	end,
+})
+
+local Button = T2:CreateButton({
+	Name = "Gator Esp",
+	Callback = function()
+        for i,v in pairs(game.Workspace:GetDescendants()) do
+            if v.ClassName == "Model" and v.Parent.Name == "Gator" then
+                local BillboardGui = Instance.new("Highlight")
+                BillboardGui.Parent = v.Parent
             end
         end
 	end,
