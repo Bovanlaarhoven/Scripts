daa9c74b59264aed61daae802072965c925654b0
Bo <bo.van.laarhoven@gmail.com>
2022-10-29 11:10:01 +0200

    Update the wild west.lua

--- the wild west.lua
+++ the wild west.lua
@@ -40,7 +40,7 @@ local Button = T1:CreateButton({
             TextLabel.BackgroundTransparency = 1
             TextLabel.Size = UDim2.new(1, 0, 1, 0)
             TextLabel.Text = "Bear"
-            TextLabel.TextColor3 = Color3.new(1, 0, 0)
+            TextLabel.TextColor3 = Color3.new(0.545098, 0.345098, 0.003921)
             TextLabel.TextScaled = true
             end
         end
@@ -66,7 +66,7 @@ local Button = T1:CreateButton({
             TextLabel.BackgroundTransparency = 1
             TextLabel.Size = UDim2.new(1, 0, 1, 0)
             TextLabel.Text = "Bison"
-            TextLabel.TextColor3 = Color3.new(1, 0, 0)
+            TextLabel.TextColor3 = Color3.new(0.831372, 0.607843, 0.607843)
             TextLabel.TextScaled = true
             end
         end
@@ -92,7 +92,7 @@ local Button = T1:CreateButton({
             TextLabel.BackgroundTransparency = 1
             TextLabel.Size = UDim2.new(1, 0, 1, 0)
             TextLabel.Text = "Deer"
-            TextLabel.TextColor3 = Color3.new(1, 0, 0)
+            TextLabel.TextColor3 = Color3.new(0.235294, 0.125490, 0.125490)
             TextLabel.TextScaled = true
             end
         end
@@ -118,7 +118,33 @@ local Button = T1:CreateButton({
             TextLabel.BackgroundTransparency = 1
             TextLabel.Size = UDim2.new(1, 0, 1, 0)
             TextLabel.Text = "Gator"
-            TextLabel.TextColor3 = Color3.new(1, 0, 0)
+            TextLabel.TextColor3 = Color3.new(0, 1, 0.117647)
+            TextLabel.TextScaled = true
+            end
+        end
+	end,
+})
+
+local Button = T2:CreateButton({
+	Name = "Coal Esp",
+	Callback = function()
+        for i,v in pairs(game.Workspace:GetDescendants()) do
+            if v.ClassName == "Model" and v.Parent.Name == "1" or v.Parent.Name == "2" or v.Parent.Name == "3" or v.Parent.Name == "4" then
+            local BillboardGui = Instance.new("BillboardGui")
+            local TextLabel = Instance.new("TextLabel")
+            
+            BillboardGui.Parent = v.Parent
+            BillboardGui.AlwaysOnTop = true
+            BillboardGui.LightInfluence = 1
+            BillboardGui.Size = UDim2.new(0, 50, 0, 50)
+            BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
+            
+            TextLabel.Parent = BillboardGui
+            TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+            TextLabel.BackgroundTransparency = 1
+            TextLabel.Size = UDim2.new(1, 0, 1, 0)
+            TextLabel.Text = "Coal"
+            TextLabel.TextColor3 = Color3.new(0, 0, 0)
             TextLabel.TextScaled = true
             end
         end
