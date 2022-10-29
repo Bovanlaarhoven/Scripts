035b51c448fe0426474e59dd1b7284000d0b35b4
Bo <bo.van.laarhoven@gmail.com>
2022-10-30 00:52:32 +0200

    Update the wild west.lua

--- the wild west.lua
+++ the wild west.lua
@@ -20,6 +20,7 @@ local Window = Rayfield:CreateWindow({
 
 local T1 = Window:CreateTab("Animal Esp(billboard)")
 local T2 = Window:CreateTab("Animal Esp(highlight)")
+local T3 = Window:CreateTab("Other")
 
 local Button = T1:CreateButton({
 	Name = "Bear Esp",
@@ -181,6 +182,34 @@ local Button = T2:CreateButton({
                 if v.ClassName == "Model" and v.Parent.Name == "Gator" then
                     local BillboardGui = Instance.new("Highlight")
                     BillboardGui.Parent = v.Parent
+                end
+            end
+        end)
+	end,
+})
+
+local Button = T3:CreateButton({
+	Name = "Struken tree Esp",
+	Callback = function()
+        game:GetService("Workspace")["WORKSPACE_Entities"].Animals.ChildAdded:Connect(function(child)
+            for i,v in pairs(game.Workspace:GetDescendants()) do
+                if v.ClassName == "ParticleEmitter" and v.Parent.Name == "Strike2" then
+                local BillboardGui = Instance.new("BillboardGui")
+                local TextLabel = Instance.new("TextLabel")
+                
+                BillboardGui.Parent = v.Parent
+                BillboardGui.AlwaysOnTop = true
+                BillboardGui.LightInfluence = 1
+                BillboardGui.Size = UDim2.new(0, 50, 0, 50)
+                BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
+                
+                TextLabel.Parent = BillboardGui
+                TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+                TextLabel.BackgroundTransparency = 1
+                TextLabel.Size = UDim2.new(1, 0, 1, 0)
+                TextLabel.Text = "Tree"
+                TextLabel.TextColor3 = Color3.new(0, 1, 0.117647)
+                TextLabel.TextScaled = true
                 end
             end
         end)
