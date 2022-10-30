bef7e4cf4b0fb9d5b1e174ef86fe72a3a5c07901
Bo <bo.van.laarhoven@gmail.com>
2022-10-30 11:46:21 +0100

    Update the wild west.lua

--- the wild west.lua
+++ the wild west.lua
@@ -1,4 +1,5 @@
 getgenv().SecureMode = true
+local Light = game:GetService("Lighting")
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
 local Window = Rayfield:CreateWindow({
@@ -42,7 +43,7 @@ local Button = T1:CreateButton({
                 TextLabel.BackgroundTransparency = 1
                 TextLabel.Size = UDim2.new(1, 0, 1, 0)
                 TextLabel.Text = "Bear"
-                TextLabel.TextColor3 = Color3.new(0.545098, 0.345098, 0.003921)
+                TextLabel.TextColor3 = Color3.new(0.831372, 0.607843, 0.607843)
                 TextLabel.TextScaled = true
                 end
             end
@@ -189,41 +190,11 @@ local Button = T2:CreateButton({
 })
 
 local Button = T3:CreateButton({
-	Name = "Bear Esp",
+	Name = "Horse Esp",
 	Callback = function()
         game:GetService("Workspace")["WORKSPACE_Entities"].Animals.ChildAdded:Connect(function(child)
             for i,v in pairs(game.Workspace:GetDescendants()) do
-                if v.ClassName == "Model" and v.Parent.Name == "Bear" then
-                    if v.ClassName == "NumberValue" and v.Health.NumberValue > 800 then
-                        local BillboardGui = Instance.new("BillboardGui")
-                        local TextLabel = Instance.new("TextLabel")
-                        
-                        BillboardGui.Parent = v.Parent
-                        BillboardGui.AlwaysOnTop = true
-                        BillboardGui.LightInfluence = 1
-                        BillboardGui.Size = UDim2.new(0, 50, 0, 50)
-                        BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
-                        
-                        TextLabel.Parent = BillboardGui
-                        TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
-                        TextLabel.BackgroundTransparency = 1
-                        TextLabel.Size = UDim2.new(1, 0, 1, 0)
-                        TextLabel.Text = "Bear"
-                        TextLabel.TextColor3 = Color3.new(0.545098, 0.345098, 0.003921)
-                        TextLabel.TextScaled = true
-                    end
-                end
-            end
-        end)
-	end,
-})
-
-local Button = T3:CreateButton({
-	Name = "Button Example",
-	Callback = function()
-        for i,v in pairs(game:GetService("Workspace")["WORKSPACE_Entities"].Animals:GetChildren()) do
-            local health = v:WaitForChild("Health")
-            if health and health.Value > 800 then 
+                if v.ClassName == "Model" and v.Parent.Name == "Horse" then
                 local BillboardGui = Instance.new("BillboardGui")
                 local TextLabel = Instance.new("TextLabel")
                 
@@ -237,11 +208,55 @@ local Button = T3:CreateButton({
                 TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
                 TextLabel.BackgroundTransparency = 1
                 TextLabel.Size = UDim2.new(1, 0, 1, 0)
-                TextLabel.Text = "Bear"
-                TextLabel.TextColor3 = Color3.new(0.545098, 0.345098, 0.003921)
+                TextLabel.Text = "Horse"
+                TextLabel.TextColor3 = Color3.new(0, 1, 0.117647)
                 TextLabel.TextScaled = true
+                end
             end
-        end
+        end)
+	end,
+})
+
+local Button = T3:CreateButton({
+	Name = "Horse Esp",
+	Callback = function()
+        game:GetService("Workspace")["WORKSPACE_Entities"].Animals.ChildAdded:Connect(function(child)
+            for i,v in pairs(game.Workspace:GetDescendants()) do
+                if v.ClassName == "Model" and v.Parent.Name == "Horse" then
+                    local BillboardGui = Instance.new("Highlight")
+                    BillboardGui.Parent = v.Parent
+                end
+            end
+        end)
+	end,
+})
+
+
+local Button = T3:CreateButton({
+	Name = "Item Esp",
+	Callback = function()
+        game:GetService("Workspace").Ignore.ChildAdded:Connect(function(child)
+            for i,v in pairs(game.Workspace:GetDescendants()) do
+                if v.ClassName == "Model" and v.Parent.Name == "AnimalMeat" or v.Parent.Name == "GrizzlyPelt" or v.Parent.Name == "BisonPelt" or v.Parent.Name == "BearClaw" or v.Parent.Name == "DeerPelt" then
+                    local BillboardGui = Instance.new("Highlight")
+                    BillboardGui.Parent = v.Parent
+                end
+            end
+        end)
+	end,
+})
+
+local Button = T3:CreateButton({
+	Name = "Fullbright",
+	Callback = function()
+        Light.Ambient = Color3.new(1, 1, 1)
+        Light.ColorShift_Bottom = Color3.new(1, 1, 1)
+        Light.ColorShift_Top = Color3.new(1, 1, 1)
+        game.Lighting.FogEnd = 100000
+        game.Lighting.FogStart = 0
+        game.Lighting.ClockTime = 14
+        game.Lighting.Brightness = 2
+        game.Lighting.GlobalShadows = false
 	end,
 })
 
