9c489fc77e54f452d3cba5439cab33482aca797e
Bo <bo.van.laarhoven@gmail.com>
2022-11-02 09:46:30 +0100

    Update test4.lua

--- test4.lua
+++ test4.lua
@@ -6,17 +6,20 @@ mouse.move:Connect(function()
 	local closestplr
 	local closestpos = 1e+100
 	for i,v in pairs(game.Players:GetPlayers()) do
-		if v.Character then
-			local mag = (mouse.Hit.p - v.Character.HumanoidRootPart.CFrame.Position).Magnitude
-			if  mag < closestpos then
-				closestplr = v
-				closestpos = mag
-			end
-		end
-	end
-	
-	if closestplr then
-		--print(closestplr.Name)
-        highlight.Parent = closestplr.Character.Head
-	end
+        if v.Character ~= game.Players.LocalPlayer.Character then
+            if v.Character then
+                local mag = (mouse.Hit.p - v.Character.HumanoidRootPart.CFrame.Position).Magnitude
+                if  mag < closestpos then
+                    closestplr = v
+                    closestpos = mag
+                end
+            end
+        end
+        
+        if closestplr then
+            highlight.Parent = closestplr.Character
+            highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+            highlight.FillColor = Color3.new(0.917647, 0, 1)
+        end
+    end
 end)
