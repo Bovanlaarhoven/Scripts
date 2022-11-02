d6a649a5249a5d8038b929aecc791f18946b3152
Bo <bo.van.laarhoven@gmail.com>
2022-11-02 16:47:33 +0100

    Update juul.lua

--- juul.lua
+++ juul.lua
@@ -1,15 +1,21 @@
 local plr = game.Players.LocalPlayer
 local mouse = plr:GetMouse()
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
+local Gui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
-local Username = Instance.new("TextLabel")
-local Distance = Instance.new("TextLabel")
-local HP = Instance.new("TextLabel")
-local Bar = Instance.new("Frame")
+local UIGradient = Instance.new("UIGradient")
+local TextLabel = Instance.new("TextLabel")
+local Extra = Instance.new("Frame")
 local UICorner_2 = Instance.new("UICorner")
-local Bar_2 = Instance.new("Frame")
+local Lookingat = Instance.new("TextLabel")
+local Under = Instance.new("Frame")
 local UICorner_3 = Instance.new("UICorner")
+local Bar = Instance.new("Frame")
+local UICorner_4 = Instance.new("UICorner")
+local _100 = Instance.new("TextLabel")
+local Health = Instance.new("TextLabel")
+local Arrow = Instance.new("ImageLabel")
+local Distance = Instance.new("TextLabel")
 local highlight = Instance.new("Highlight")
 
 mouse.move:Connect(function()
@@ -30,80 +36,141 @@ mouse.move:Connect(function()
             highlight.Parent = closestplr.Character
             highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
             highlight.FillColor = Color3.new(0.917647, 0, 1)
-            ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
-            Frame.Parent = ScreenGui
-            Frame.BackgroundColor3 = Color3.fromRGB(99, 99, 99)
-            Frame.Position = UDim2.new(0.290521622, 0, 0.437731653, 0)
-            Frame.Size = UDim2.new(0, 242, 0, 109)
+            Gui.Name = "Gui"
+            Gui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+            Gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+        
+            Main.Name = "Main"
+            Main.Parent = Gui
+            Main.BackgroundColor3 = Color3.fromRGB(84, 85, 86)
+            Main.BackgroundTransparency = 0.050
+            Main.BorderColor3 = Color3.fromRGB(84, 85, 86)
+            Main.Position = UDim2.new(0.41379407, 0, 0.058997184, 0)
+            Main.Size = UDim2.new(0, 273, 0, 92)
+            Main.ZIndex = 2
             
-            UICorner.Parent = Frame
+            UICorner.Parent = Main
             
-            Username.Name = "Username"
-            Username.Parent = Frame
-            Username.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-            Username.BackgroundTransparency = 1.000
-            Username.Position = UDim2.new(0.0316300839, 0, 0.0630406365, 0)
-            Username.Size = UDim2.new(0, 95, 0, 46)
-            Username.Font = Enum.Font.SourceSans
-            Username.Text = closestplr.Name..""
-            Username.TextColor3 = Color3.fromRGB(255, 255, 255)
-            Username.TextScaled = true
-            Username.TextSize = 14.000
-            Username.TextWrapped = true
+            UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(207, 212, 221)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(207, 212, 221))}
+            UIGradient.Transparency = NumberSequence.new{NumberSequenceKeypoint.new(0.00, 0.10), NumberSequenceKeypoint.new(1.00, 0.10)}
+            UIGradient.Parent = Main
             
-            Distance.Name = "Distance"
-            Distance.Parent = Frame
-            Distance.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-            Distance.BackgroundTransparency = 1.000
-            Distance.Position = UDim2.new(0.745601654, 0, 0.127260804, 0)
-            Distance.Size = UDim2.new(0, 51, 0, 46)
-            Distance.Font = Enum.Font.SourceSans
-            Distance.Text = closestpos..""
-            Distance.TextColor3 = Color3.fromRGB(255, 255, 255)
-            Distance.TextScaled = true
-            Distance.TextSize = 14.000
-            Distance.TextWrapped = false
+            TextLabel.Parent = Main
+            TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            TextLabel.BackgroundTransparency = 1.000
+            TextLabel.Position = UDim2.new(0, 0, 0.00653625466, 0)
+            TextLabel.Size = UDim2.new(0, 133, 0, 64)
+            TextLabel.Font = Enum.Font.SourceSans
+            TextLabel.Text = closestplr.Name..""
+            TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+            TextLabel.TextSize = 25.000
             
-            HP.Name = "HP"
-            HP.Parent = Frame
-            HP.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-            HP.BackgroundTransparency = 1.000
-            HP.Position = UDim2.new(0.743810534, 0, 0.552559078, 0)
-            HP.Size = UDim2.new(0, 56, 0, 51)
-            HP.Font = Enum.Font.SourceSans
-            HP.Text = closestplr.Character.Humanoid.Health.."/100"
-            HP.TextColor3 = Color3.fromRGB(255, 255, 255)
-            HP.TextScaled = true
-            HP.TextSize = 14.000
-            HP.TextWrapped = true
+            Extra.Name = "Extra"
+            Extra.Parent = Gui
+            Extra.BackgroundColor3 = Color3.fromRGB(68, 70, 71)
+            Extra.BackgroundTransparency = 0.400
+            Extra.BorderColor3 = Color3.fromRGB(52, 53, 54)
+            Extra.Position = UDim2.new(0.401771754, 0, 0.0209895372, 0)
+            Extra.Size = UDim2.new(0, 159, 0, 59)
+            Extra.ZIndex = 3
+            
+            UICorner_2.Parent = Extra
+            
+            Lookingat.Name = "Looking at"
+            Lookingat.Parent = Extra
+            Lookingat.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            Lookingat.BackgroundTransparency = 1.000
+            Lookingat.Position = UDim2.new(0.0472772941, 0, 0.127101064, 0)
+            Lookingat.Size = UDim2.new(0, 142, 0, 37)
+            Lookingat.Font = Enum.Font.SourceSans
+            Lookingat.Text = "Looking at"
+            Lookingat.TextColor3 = Color3.fromRGB(255, 255, 255)
+            Lookingat.TextSize = 37.000
+            Lookingat.TextWrapped = true
+            
+            Under.Name = "Under"
+            Under.Parent = Gui
+            Under.BackgroundColor3 = Color3.fromRGB(51, 51, 51)
+            Under.BackgroundTransparency = 0.100
+            Under.BorderColor3 = Color3.fromRGB(51, 51, 51)
+            Under.Position = UDim2.new(0.419083834, 0, 0.115057558, 0)
+            Under.Size = UDim2.new(0, 167, 0, 22)
+            Under.ZIndex = 3
+            
+            UICorner_3.Parent = Under
             
             Bar.Name = "Bar"
-            Bar.Parent = ScreenGui
-            Bar.BackgroundColor3 = Color3.fromRGB(85, 85, 85)
-            Bar.Position = UDim2.new(0.298285097, 0, 0.505800188, 0)
-            Bar.Size = UDim2.new(0, 154, 0, 26)
-            UICorner_2.Parent = Bar
-            Bar_2.Name = "Bar2"
-            Bar_2.Parent = ScreenGui
-            Bar_2.BackgroundColor3 = Color3.fromRGB(110, 255, 66)
-            Bar_2.Position = UDim2.new(0.298285097, 0, 0.505800188, 0)
-            Bar_2.Size = UDim2.new(0, 154, 0, 26)
-            UICorner_3.Parent = Bar_2
+            Bar.Parent = Gui
+            Bar.BackgroundColor3 = Color3.fromRGB(77, 255, 104)
+            Bar.BackgroundTransparency = 0.100
+            Bar.BorderColor3 = Color3.fromRGB(77, 255, 104)
+            Bar.Position = UDim2.new(0.419083834, 0, 0.115057558, 0)
+            Bar.Size = UDim2.new(0, 167, 0, 22)
+            Bar.ZIndex = 3
+            
+            UICorner_4.Parent = Bar
+            
+            _100.Name = "/100"
+            _100.Parent = Gui
+            _100.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            _100.BackgroundTransparency = 1.000
+            _100.Position = UDim2.new(0.524678409, 0, 0.114537895, 0)
+            _100.Size = UDim2.new(0, 54, 0, 23)
+            _100.ZIndex = 3
+            _100.Font = Enum.Font.SourceSans
+            _100.Text = "/100"
+            _100.TextColor3 = Color3.fromRGB(255, 255, 255)
+            _100.TextSize = 25.000
+            _100.TextWrapped = true
+            
+            Health.Name = "Health"
+            Health.Parent = Gui
+            Health.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            Health.BackgroundTransparency = 1.000
+            Health.Position = UDim2.new(0.505883515, 0, 0.106204569, 0)
+            Health.Size = UDim2.new(0, 54, 0, 23)
+            Health.ZIndex = 3
+            Health.Font = Enum.Font.SourceSans
+            Health.Text = math.floor(closestplr.Character.Humanoid.Health.."")
+            Health.TextColor3 = Color3.fromRGB(255, 255, 255)
+            Health.TextSize = 30.000
+            Health.TextWrapped = true
+            
+            Arrow.Name = "Arrow"
+            Arrow.Parent = Gui
+            Arrow.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            Arrow.BackgroundTransparency = 1.000
+            Arrow.Position = UDim2.new(0.507819235, 0, 0.068454653, 0)
+            Arrow.Rotation = 270.000
+            Arrow.Size = UDim2.new(0, 47, 0, 38)
+            Arrow.ZIndex = 3
+            Arrow.Image = "http://www.roblox.com/asset/?id=11449963148"
+            
+            Distance.Name = "Distance"
+            Distance.Parent = Gui
+            Distance.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            Distance.BackgroundTransparency = 1.000
+            Distance.Position = UDim2.new(0.531131506, 0, 0.068454653, 0)
+            Distance.Size = UDim2.new(0, 41, 0, 41)
+            Distance.ZIndex = 3
+            Distance.Font = Enum.Font.SourceSans
+            Distance.Text = math.floor(closestpos.."")
+            Distance.TextColor3 = Color3.fromRGB(255, 255, 255)
+            Distance.TextSize = 30.000
+            Distance.TextWrapped = true
 
             if closestplr.Character.Humanoid.Health < 50 then
-                Bar_2.BackgroundColor3 = Color3.fromRGB(255, 162, 0)
-                Bar_2.Size = UDim2.new(0, 77, 0, 26)
+                Bar.BackgroundColor3 = Color3.fromRGB(255, 162, 0)
+                Bar.Size = UDim2.new(0, 77, 0, 26)
             end
 
             if closestplr.Character.Humanoid.Health < 20 then
-                Bar_2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-                Bar_2.Size = UDim2.new(0, 30, 0, 26)
+                Bar.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+                Bar.Size = UDim2.new(0, 30, 0, 26)
             end
 
             if closestplr.Character.Humanoid.Health == 0 then
-                Bar_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-                Bar_2.Size = UDim2.new(0, 0, 0, 26)
+                Bar.BackgroundTransparency = 1
             end
 
         end
