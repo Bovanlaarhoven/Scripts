b30861f775f2481c5e04c277d0d2a66cad5f2232
Bo <bo.van.laarhoven@gmail.com>
2022-11-09 20:43:23 +0100

    Update Clientv2.lua

--- Clientv2.lua
+++ Clientv2.lua
@@ -464,105 +464,101 @@ mouse.move:Connect(function()
             
             UICorner_15.CornerRadius = UDim.new(0, 5)
             UICorner_15.Parent = OpenClose
-            
-            -- Scripts:
+
+        local script = Instance.new('LocalScript', Top)
+    
+        local UserInputService = game:GetService("UserInputService")
+        local runService = (game:GetService("RunService"));
         
-                local script = Instance.new('LocalScript', Top)
-            
-                local UserInputService = game:GetService("UserInputService")
-                local runService = (game:GetService("RunService"));
-                
-                local gui = script.Parent
-                
-                local dragging
-                local dragInput
-                local dragStart
-                local startPos
-                
-                function Lerp(a, b, m)
-                    return a + (b - a) * m
-                end;
-                
-                local lastMousePos
-                local lastGoalPos
-                local DRAG_SPEED = (25);
-                function Update(dt)
-                    if not (startPos) then return end;
-                    if not (dragging) and (lastGoalPos) then
-                        gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
-                        return 
-                    end;
-                
-                    local delta = (lastMousePos - UserInputService:GetMouseLocation())
-                    local xGoal = (startPos.X.Offset - delta.X);
-                    local yGoal = (startPos.Y.Offset - delta.Y);
-                    lastGoalPos = UDim2.new(startPos.X.Scale, xGoal, startPos.Y.Scale, yGoal)
-                    gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
-                end;
-                
-                gui.InputBegan:Connect(function(input)
-                    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-                        dragging = true
-                        dragStart = input.Position
-                        startPos = gui.Position
-                        lastMousePos = UserInputService:GetMouseLocation()
-                
-                        input.Changed:Connect(function()
-                            if input.UserInputState == Enum.UserInputState.End then
-                                dragging = false
-                            end
-                        end)
-                    end
-                end)
-                
-                gui.InputChanged:Connect(function(input)
-                    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-                        dragInput = input
-                    end
-                end)
-                
-                runService.Heartbeat:Connect(Update)
-            end
-                local script = Instance.new('LocalScript', OpenClose)
-            
-                local Toggled = true
-                
-                OpenClose.MouseButton1Down:Connect(function()
-                    if Toggled then
-                        Toggled = false
-                        Background:TweenSize(UDim2.new(0, 317,0, 0), "InOut", "Linear", 0.1)
-                        AccountAge.Visible = false
-                        Hp.Visible = false
-                        Extra.Visible = false
-                        Health.Visible = false
-                        JumpPower.Visible = false
-                        WalkSpeed.Visible = false
-                        MaxHealth.Visible = false
-                        Player.Visible = false
-                        RigType.Visible = false
-                        UserId.Visible = false
-                        Username.Visible = false
-                        OpenClose.BackgroundColor3 = Color3.fromRGB(34, 255, 10)
-                        OpenClose.Text = "Open"
-                    else
-                        Toggled = true
-                        Background:TweenSize(UDim2.new(0, 317,0, 308), "InOut", "Linear", 0.1)
-                        AccountAge.Visible = true
-                        Hp.Visible = true
-                        Extra.Visible = true
-                        Health.Visible = true
-                        JumpPower.Visible = true
-                        WalkSpeed.Visible = true
-                        MaxHealth.Visible = true
-                        Player.Visible = true
-                        RigType.Visible = true
-                        UserId.Visible = true
-                        Username.Visible = true
-                        OpenClose.BackgroundColor3 = Color3.fromRGB(218, 24, 18)
-                        OpenClose.Text = "Close"
+        local gui = script.Parent
+        
+        local dragging
+        local dragInput
+        local dragStart
+        local startPos
+        
+        function Lerp(a, b, m)
+            return a + (b - a) * m
+        end;
+        
+        local lastMousePos
+        local lastGoalPos
+        local DRAG_SPEED = (25);
+        function Update(dt)
+            if not (startPos) then return end;
+            if not (dragging) and (lastGoalPos) then
+                gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
+                return 
+            end;
+        
+            local delta = (lastMousePos - UserInputService:GetMouseLocation())
+            local xGoal = (startPos.X.Offset - delta.X);
+            local yGoal = (startPos.Y.Offset - delta.Y);
+            lastGoalPos = UDim2.new(startPos.X.Scale, xGoal, startPos.Y.Scale, yGoal)
+            gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
+        end;
+        
+        gui.InputBegan:Connect(function(input)
+            if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+                dragging = true
+                dragStart = input.Position
+                startPos = gui.Position
+                lastMousePos = UserInputService:GetMouseLocation()
+        
+                input.Changed:Connect(function()
+                    if input.UserInputState == Enum.UserInputState.End then
+                        dragging = false
                     end
                 end)
             end
-        end
+        end)
+        
+        gui.InputChanged:Connect(function(input)
+            if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+                dragInput = input
+            end
+        end)
+        
+        runService.Heartbeat:Connect(Update)
+    end
+        local script = Instance.new('LocalScript', OpenClose)
+    
+        local Toggled = true
+        
+        OpenClose.MouseButton1Down:Connect(function()
+            if Toggled then
+                Toggled = false
+                Background:TweenSize(UDim2.new(0, 317,0, 0), "InOut", "Linear", 0.1)
+                AccountAge.Visible = false
+                Hp.Visible = false
+                Extra.Visible = false
+                Health.Visible = false
+                JumpPower.Visible = false
+                WalkSpeed.Visible = false
+                MaxHealth.Visible = false
+                Player.Visible = false
+                RigType.Visible = false
+                UserId.Visible = false
+                Username.Visible = false
+                OpenClose.BackgroundColor3 = Color3.fromRGB(34, 255, 10)
+                OpenClose.Text = "Open"
+            else
+                Toggled = true
+                Background:TweenSize(UDim2.new(0, 317,0, 308), "InOut", "Linear", 0.1)
+                AccountAge.Visible = true
+                Hp.Visible = true
+                Extra.Visible = true
+                Health.Visible = true
+                JumpPower.Visible = true
+                WalkSpeed.Visible = true
+                MaxHealth.Visible = true
+                Player.Visible = true
+                RigType.Visible = true
+                UserId.Visible = true
+                Username.Visible = true
+                OpenClose.BackgroundColor3 = Color3.fromRGB(218, 24, 18)
+                OpenClose.Text = "Close"
+            end
+        end)
     end
 end)
