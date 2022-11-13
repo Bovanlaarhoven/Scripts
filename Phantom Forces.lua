d0f2220b7519b8971d62e49c3699faa22c21f097
Bo <bo.van.laarhoven@gmail.com>
2022-11-13 21:15:33 +0100

    noob

--- Phantom Forces.lua
+++ Phantom Forces.lua
@@ -2,11 +2,10 @@ local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
-getgenv().enabled = false
-getgenv().OutlineColor = Color3.fromRGB(255, 255, 255)
-getgenv().FillColor = Color3.fromRGB(255, 255, 255)
-getgenv().FillTransparency = 1
-getgenv().OutlineTransparency = 1
+local enabled = true
+local highlight = Instance.new("Highlight")
+local plr = game:GetService("Players").LocalPlayer
+local players = game:GetService("Players")
 
 local Window = Library:CreateWindow({
     Title = '[Phantom Forces] atomic V0.01 By hydra#8270',
@@ -33,61 +32,32 @@ Toggles.ESP:OnChanged(function()
     enabled = Toggles.ESP.Value
 end)
 
-ESP:AddLabel('Fill Color'):AddColorPicker('FillColor', {
-    Default = Color3.new(0, 1, 0),
-    Title = 'Some color',
-})
-
-Options.FillColor:OnChanged(function()
-    FillColor = Options.FillColor.Value
-end)
-
-ESP:AddLabel('Outline Color'):AddColorPicker('OutlineColor', {
-    Default = Color3.new(0, 1, 0),
-    Title = 'Some color',
-})
-
-Options.OutlineColor:OnChanged(function()
-    OutlineColor = Options.OutlineColor.Value
-end)
-
-pcall(
-    function()
-        local a = Instance.new("Highlight")
-        game:GetService("RunService").RenderStepped:Connect(
-            function()
-                for b, c in pairs(game:GetService("Players"):GetPlayers()) do
-                    if not c.Character:FindFirstChild("Highlight") then
-                        a.FillTransparency = 1
-                        a:Clone().Parent = c.Character
-                        a.OutlineColor = Color3.fromRGB(255, 255, 255)
-                    end
-                    game.Players.PlayerAdded:Connect(
-                        function(d)
-                            d.CharacterAdded:Connect(
-                                function(e)
-                                    if not e:FindFirstChild("Highlight") then
-                                        a.FillTransparency = 1
-                                        a:Clone().Parent = e
-                                        a.OutlineColor = Color3.fromRGB(255, 255, 255)
-                                    end
-                                end
-                            )
-                        end
-                    )
-                end
-                for b, c in pairs(game.Players:GetPlayers()) do
-                    local f = c.Character:FindFirstChild("Highlight")
-                    f.Enabled = enabled
-                    f.OutlineColor = OutlineColor
-                    f.FillColor = FillColor
-                    f.OutlineTransparency = OutlineTransparency
-                    f.FillTransparency = FillTransparency
-                end
-            end
-        )
+if enabled then
+    for i,v in pairs(game:GetService("Players"):GetChildren()) do
+        local highlightclone = highlight:Clone()
+        highlightclone.Adornee = v.Character
+        highlightclone.Parent = v.Character
+        highlightclone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
     end
-)
+end
+
+players.PlayerAdded:Connect(function()
+    for i,v in pairs(game:GetService("Players"):GetChildren()) do
+        local highlightclone = highlight:Clone()
+        highlightclone.Adornee = v.Character
+        highlightclone.Parent = v.Character
+        highlightclone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+    end
+end)
+
+players.PlayerRemoving:Connect(function(playerRemoved)
+    for i,v in pairs(game:GetService("Players"):GetChildren()) do
+        if v == playerRemoved then
+            v.Character:FindFirstChildOfClass("Highlight"):Destroy()
+        end
+    end
+end)
+
 
 local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
 MenuGroup:AddButton('Unload', function() Library:Unload() end)
