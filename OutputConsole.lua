5d2e575aba919ca4acd7be1fdf488d437185be9f
Bo <bo.van.laarhoven@gmail.com>
2022-11-17 23:23:32 +0100

    Update OutputConsole.lua

--- OutputConsole.lua
+++ OutputConsole.lua
@@ -33,6 +33,15 @@ local UICorner_3 = Instance.new("UICorner")
 local TextButton = Instance.new("TextButton")
 local Title1 = Instance.new("TextLabel")
 
+local PlayerList = Instance.new("ScreenGui")
+local Filling = Instance.new("Frame")
+local UICorner4 = Instance.new("UICorner")
+local padsing = Instance.new("Frame")
+local UIListLayout2 = Instance.new("UIListLayout")
+local UICorner_5 = Instance.new("UICorner")
+local TextButton1 = Instance.new("TextButton")
+local Title4 = Instance.new("TextLabel")
+
 Logger.Name = "Logger"
 Logger.Parent = CoreGui
 Logger.ResetOnSpawn = false
@@ -320,10 +329,118 @@ local function BMRDXRL_fake_script() -- Idsfilling.Dragify
 end
 coroutine.wrap(BMRDXRL_fake_script)()
 
+PlayerList.Name = "PlayerList"
+PlayerList.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+PlayerList.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Filling.Name = "Filling"
+Filling.Parent = PlayerList
+Filling.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+Filling.Position = UDim2.new(0.00622848049, 0, 0.0266821347, 0)
+Filling.Size = UDim2.new(0, 261, 0, 816)
+
+UICorner4.CornerRadius = UDim.new(0, 6)
+UICorner4.Parent = Filling
+
+padsing.Name = "padsing"
+padsing.Parent = Filling
+padsing.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+padsing.Position = UDim2.new(0.0920589641, 0, 0.0853287503, 0)
+padsing.Size = UDim2.new(0, 212, 0, 704)
+
+UIListLayout2.Parent = padsing
+
+UICorner_5.CornerRadius = UDim.new(0, 6)
+UICorner_5.Parent = padsing
+
+TextButton1.Parent = padsing
+TextButton1.BackgroundColor3 = Color3.fromRGB(49, 49, 49)
+TextButton1.BorderColor3 = Color3.fromRGB(35, 35, 35)
+TextButton1.Size = UDim2.new(0, 212, 0, 50)
+TextButton1.ZIndex = 2
+TextButton1.Font = Enum.Font.SourceSansBold
+TextButton1.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton1.TextSize = 18.000
+
+Title4.Name = "Title4"
+Title4.Parent = Filling
+Title4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Title4.BackgroundTransparency = 1.000
+Title4.Position = UDim2.new(0.354396492, 0, 0.0354893431, 0)
+Title4.Size = UDim2.new(0, 75, 0, 25)
+Title4.Font = Enum.Font.GothamBold
+Title4.Text = "Player List"
+Title4.TextColor3 = Color3.fromRGB(255, 255, 255)
+Title4.TextSize = 14.000
+Title4.TextXAlignment = Enum.TextXAlignment.Left
+
+-- Scripts:
+
+local function DQVSVDW_fake_script() -- Filling.Dragify 
+	local script = Instance.new('LocalScript', Filling)
+
+	local UserInputService = game:GetService("UserInputService")
+	local runService = (game:GetService("RunService"));
+	
+	local gui = script.Parent
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	function Lerp(a, b, m)
+		return a + (b - a) * m
+	end;
+	
+	local lastMousePos
+	local lastGoalPos
+	local DRAG_SPEED = (10);
+	function Update(dt)
+		if not (startPos) then return end;
+		if not (dragging) and (lastGoalPos) then
+			gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
+			return 
+		end;
+	
+		local delta = (lastMousePos - UserInputService:GetMouseLocation())
+		local xGoal = (startPos.X.Offset - delta.X);
+		local yGoal = (startPos.Y.Offset - delta.Y);
+		lastGoalPos = UDim2.new(startPos.X.Scale, xGoal, startPos.Y.Scale, yGoal)
+		gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
+	end;
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+			lastMousePos = UserInputService:GetMouseLocation()
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	
+	runService.Heartbeat:Connect(Update)
+end
+coroutine.wrap(DQVSVDW_fake_script)()
+
+
 InputService.InputBegan:Connect(function(Input, Processed)
     if Input.KeyCode == Enum.KeyCode.F1 or (Input.KeyCode == Enum.KeyCode.Insert and (not Processed)) then
         Main.Visible = not Main.Visible
 		Idsfilling.Visible = not Idsfilling.Visible
+		Filling.Visible = not Filling.Visible
     end
 end)
 
@@ -331,6 +448,7 @@ InputService.InputBegan:Connect(function(Input)
 	if Input.KeyCode == Enum.KeyCode.Delete then
 		Main:Destroy()
 		Idsfilling:Destroy()
+		Filling:Destroy()
 	end
 end)
 	
