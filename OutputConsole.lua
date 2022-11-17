23cb330d23a78adfc015073b6e053110d4beee3d
Bo <bo.van.laarhoven@gmail.com>
2022-11-17 21:42:12 +0100

    Update OutputConsole.lua

--- OutputConsole.lua
+++ OutputConsole.lua
@@ -243,6 +243,65 @@ Title1.TextColor3 = Color3.fromRGB(255, 255, 255)
 Title1.TextSize = 14.000
 Title1.TextXAlignment = Enum.TextXAlignment.Left
 
+local function BMRDXRL_fake_script() 
+	local script = Instance.new('LocalScript', Idsfilling)
+
+	local UserInputService = game:GetService("UserInputService")
+	local runService = (game:GetService("RunService"));
+	
+	local gui = script.Parent
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	function Lerp(a, b, m)
+		return a + (b - a) * m
+	end;
+	
+	local lastMousePos
+	local lastGoalPos
+	local DRAG_SPEED = (8); -- // The speed of the UI darg.
+	function Update(dt)
+		if not (startPos) then return end;
+		if not (dragging) and (lastGoalPos) then
+			gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
+			return 
+		end;
+	
+		local delta = (lastMousePos - UserInputService:GetMouseLocation())
+		local xGoal = (startPos.X.Offset - delta.X);
+		local yGoal = (startPos.Y.Offset - delta.Y);
+		lastGoalPos = UDim2.new(startPos.X.Scale, xGoal, startPos.Y.Scale, yGoal)
+		gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
+	end;
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+			lastMousePos = UserInputService:GetMouseLocation()
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	
+	runService.Heartbeat:Connect(Update)
+end
+coroutine.wrap(BMRDXRL_fake_script)()
+
 InputService.InputBegan:Connect(function(Input, Processed)
     if Input.KeyCode == Enum.KeyCode.F1 or (Input.KeyCode == Enum.KeyCode.Insert and (not Processed)) then
         Main.Visible = not Main.Visible
@@ -250,4 +309,10 @@ InputService.InputBegan:Connect(function(Input, Processed)
     end
 end)
 
-
+InputService.InputBegan:Connect(function(Input)
+	if Input.KeyCode == Enum.KeyCode.Delete then
+		Main:Destroy()
+		Idsfilling:Destroy()
+	end
+end)
+	
