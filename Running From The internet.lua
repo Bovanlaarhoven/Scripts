3c3e0a12c432ab717156fb1e9f3188ace1dabb4a
Bo <bo.van.laarhoven@gmail.com>
2022-11-30 15:36:59 +0100

    Update Running From The internet.lua

--- Running From The internet.lua
+++ Running From The internet.lua
@@ -6,6 +6,11 @@ local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shle
 getgenv().Drink = false
 getgenv().Eat = false
 getgenv().Drop = false
+getgenv().autothrow = false
+
+local x = 0
+local y = 0
+local z = 0
 
 task.spawn(function()
 	while task.wait() do
@@ -30,6 +35,19 @@ task.spawn(function()
 		end
 	end
 end)
+
+task.spawn(function()
+	while task.wait() do
+		if getgenv().autothrow then
+			game:GetService("ReplicatedStorage").Assets.RemoteFunctions.Items.ThrowBomb:InvokeServer(ohVector31)
+			game:GetService("ReplicatedStorage").Assets.RemoteFunctions.Items.ThrowLandmine:InvokeServer(ohVector31)
+			game:GetService("ReplicatedStorage").Assets.RemoteFunctions.Items.ThrowCrate:InvokeServer(ohVector31)
+			game:GetService("ReplicatedStorage").Assets.RemoteFunctions.Items.ThrowBanana:InvokeServer(ohVector31)
+		end
+	end
+end)
+
+
 
 
 local Window = Rayfield:CreateWindow({
@@ -61,6 +79,7 @@ local Window = Rayfield:CreateWindow({
 local T1 = Window:CreateTab("Player", 4483362458)
 local T2 = Window:CreateTab("Power-ups", 4483362458)
 local T3 = Window:CreateTab("Fun", 4483362458)
+local T4 = Window:CreateTab("Auto-Throw", 4483362458)
 
 local TargetWalkspeed
 local Slider = T1:CreateSlider({
@@ -84,6 +103,18 @@ local Slider = T1:CreateSlider({
 	Flag = "Slider1",
 	Callback = function(JumpPower)
         game.Players.LocalPlayer.Character.Humanoid.JumpPower = JumpPower
+	end,
+})
+
+local Slider = T1:CreateSlider({
+	Name = "HipHeight Slider",
+	Range = {70, 120},
+	Increment = 1,
+	Suffix = "HipHeight",
+	CurrentValue = 70,
+	Flag = "Slider1",
+	Callback = function(View)
+		game:GetService("Workspace").CurrentCamera.FieldOfView = View
 	end,
 })
 
@@ -149,7 +180,50 @@ local Button = T1:CreateButton({
 	end,
 })
 
+local Label1 = T4:CreateLabel("x cords are 0")
+local Label2 = T4:CreateLabel("y cords are 0")
+local Label3 = T4:CreateLabel("z cords are 0")
 
+local Toggle = T4:CreateToggle({
+	Name = "Auto Throw",
+	CurrentValue = false,
+	Flag = "Toggle1",
+	Callback = function(Value)
+		autothrow = Value
+	end,
+})
+
+local Input = T4:CreateInput({
+	Name = "x cords",
+	PlaceholderText = "x",
+	RemoveTextAfterFocusLost = false,
+	Callback = function(Text)
+		x = Text
+		Label1:Set("x cords are "..x)
+	end,
+})
+
+local Input = T4:CreateInput({
+	Name = "y cords",
+	PlaceholderText = "y",
+	RemoveTextAfterFocusLost = false,
+	Callback = function(Text)
+		y = Text
+		Label2:Set("y cords are "..y)
+	end,
+})
+
+local Input = T4:CreateInput({
+	Name = "z cords",
+	PlaceholderText = "z",
+	RemoveTextAfterFocusLost = false,
+	Callback = function(Text)
+		z = Text
+		Label3:Set("z cords are "..z)
+	end,
+})
+
+local Label = T4:CreateLabel("Recommend turning on 'Disable Ragdoll'")
 
 
 
