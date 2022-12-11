3429960f7569b1d49f2625eef8725c45882a3e12
Bo <bo.van.laarhoven@gmail.com>
2022-12-11 19:30:49 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,48 +1,38 @@
-local CoreGui = game:GetService("StarterGui")
-
-local GameTable = {
-    [9498006165] = "https://raw.githubusercontent.com/Robobo2022/script/main/TappingSimulator.lua",
-    [8884334497] = "https://raw.githubusercontent.com/Robobo2022/script/main/MiningClickerSimulator.lua",
-    [10108131074] = "https://raw.githubusercontent.com/Robobo2022/script/main/MowTheLawn.lua",
-    [183364845] = "https://raw.githubusercontent.com/Robobo2022/script/main/SpeedRun4.lua",
-    [5293755937] = "https://raw.githubusercontent.com/Robobo2022/script/main/SpeedRunSimulator.lua",
-    [7047488135] = "https://raw.githubusercontent.com/Robobo2022/script/main/SpeedrunSimulator2.lua",
-    [10379662529] = "https://raw.githubusercontent.com/Robobo2022/script/main/VictoryJump.lua";
-    [9170761016] = "https://raw.githubusercontent.com/Robobo2022/script/main/MagicClicker.lua";
-    [10106105124] = "https://raw.githubusercontent.com/Robobo2022/script/main/WheatFarmingSimulator.lua";
-    [9872472334] = "https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua";
-    [10662542523] = "https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua";
-    [10324347967] = "https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua";
-    [10530697691] = "https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua";
-    [10530697691] = "https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua";
-    [10324346056] = "https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua";
-    [10808838353] = "https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua";
-    [8888615802] = "https://raw.githubusercontent.com/spookysss/RainbowFriends/main/script%20(2).lua";
-    [10118559731] = "https://raw.githubusercontent.com/Robobo2022/script/main/nico'snextbot.lua";
-}
-
-if GameTable[game.PlaceId] == nil then
-    CoreGui:SetCore("SendNotification", {
-        Title = "Game not supported :(";
-        Text = "Copying discord to clipboard... Please check the game support channel for more information.";
-        Duration = 3;
+if getgenv().Network then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Hydra Network",
+        Text = "Hydra Network has already been executed in this game"
     })
-    
-    task.delay(2, function()
-        setclipboard("https://discord.gg/g95ZbGH4M8")
-            CoreGui:SetCore("SendNotification", {
-            Title = "Loading";
-            Text = "Game was not supported so its now loading Hydra Network Universal.";
-            Duration = 3;
+    return
+end
+
+getgenv().Network = true
+getgenv().nonSimulator = true
+getgenv().Simulator = true
+
+local ownerId = game.CreatorId
+game.Players.PlayerAdded:Connect(function(player)
+    if player.UserId == ownerId then
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Hydra Network",
+            Text = "Owner joined! be careful with what you do."
         })
-        loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/Universal.lua"), true))()
-    end)
-else
-    CoreGui:SetCore("SendNotification", {
-        Title = "Discord";
-        Text = "We would appreciate if you joined our discord (Copied)";
-        Duration = 10;
-    })
-    setclipboard("https://discord.gg/g95ZbGH4M8")
-    loadstring(game:HttpGet(GameTable[game.PlaceId]))()
+    end
+end)
+
+if nonSimulator == true then
+    local NonSimulator ({
+        [9872472334] = "Evade.lua",
+        [10324346056] = "Evade.lua",
+        [10324347967] = "Evade.lua",
+        [10539706691] = "Evade.lua",
+        [10662542523] = "Evade.lua",
+        [10808838353] = "Evade.lua",
+        [11353528705] = "Evade.lua",
+        [11353532384] = "Evade.lua",
+    })[game.PlaceId]
+
+    if NonSimulator then
+        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Robobo2022/Hydra-Network-V3/main/Games/" .. NonSimulator))()
+    end
 end
