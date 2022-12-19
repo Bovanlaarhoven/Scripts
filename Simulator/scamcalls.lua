2b8395a0c73c6e2fd1491ef81aaf1510820a08e3
Bo <bo.van.laarhoven@gmail.com>
2022-12-19 17:47:48 +0100

    Update scamcalls.lua

--- Simulator/scamcalls.lua
+++ Simulator/scamcalls.lua
@@ -1,3 +1,15 @@
+repeat wait() until game:IsLoaded()
+
+for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
+    if v.Name == "Claim" and v.ClassName == "Part" and v.Transparency == 0 then
+       game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+    end
+end
+
+game:GetService("Players").LocalPlayer.PlayerGui.PCGUI.Frame.Upgrades.Update:Fire()
+game:GetService("ReplicatedStorage").Events.GenerateNumber:FireServer()
+wait(2)
+
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/CustomFIeld/main/RayField.lua'))()
 
 local Window = Rayfield:CreateWindow({
@@ -25,3 +37,45 @@ local Window = Rayfield:CreateWindow({
        Key = "Hello"
     }
  })
+
+game:GetService("Players").LocalPlayer.PlayerGui.Notifications.Enabled = false
+local value = game:GetService("Players").LocalPlayer.GeneratedNumber.Value
+
+local Tab = Window:CreateTab("Auto Farm", 4483362458)
+
+getgenv().AutoFarm = false
+
+task.spawn(function()
+	while task.wait() do
+		if getgenv().AutoFarm == true then
+            local number = game:GetService("Players").LocalPlayer.GeneratedNumber.Value
+            local ohNumber1 = (number)
+            game:GetService("ReplicatedStorage").Events.CheckNumber:FireServer(ohNumber1)
+		end
+	end
+end)
+
+local Toggle = Tab:CreateToggle({
+   Name = "Scam call Auto Farm",
+   Info = "Does the scam call for you",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(Value)
+        AutoFarm = Value
+   end,
+})
+
+
+
+value:GetPropertyChangedSignal("Value"):Connect(function()
+Rayfield:Notify({
+	Title = "Generated Number...",
+	Content = value.Value.." is the new bank number",
+	Duration = 6.5,
+	Image = 4483362458,
+	Actions = {
+		Ignore = {
+		},
+	},
+})
+end)
