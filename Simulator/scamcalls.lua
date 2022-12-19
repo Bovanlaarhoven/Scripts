f077c45e583fe7fc4dada40ff82dc6cc6f4b6f79
Bo <bo.van.laarhoven@gmail.com>
2022-12-19 16:53:41 +0100

    Create scamcalls.lua

---
+++ Simulator/scamcalls.lua
@@ -0,0 +1,27 @@
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/CustomFIeld/main/RayField.lua'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "making scam calls to save your best friend tycoon",
+    LoadingTitle = "Hydra Network",
+    LoadingSubtitle = "by Hydra#8270",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil,
+       FileName = "Big Hub"
+    },
+    Discord = {
+       Enabled = true,
+       Invite = "YvwEyH2W6t",
+       RememberJoins = true
+    },
+    KeySystem = false,
+    KeySettings = {
+       Title = "Sirius Hub",
+       Subtitle = "Key System",
+       Note = "Join the discord (discord.gg/sirius)",
+       FileName = "SiriusKey",
+       SaveKey = true,
+       GrabKeyFromSite = false, 
+       Key = "Hello"
+    }
+ })
