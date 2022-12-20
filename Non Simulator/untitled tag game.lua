308a012d18ee902e12689d6f5a013733820e6eef
Bo <bo.van.laarhoven@gmail.com>
2022-12-20 19:06:29 +0100

    Create untitled tag game.lua

---
+++ Non Simulator/untitled tag game.lua
@@ -0,0 +1,44 @@
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/CustomFIeld/main/RayField.lua'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "untitled tag game",
+    LoadingTitle = "Hydra Network",
+    LoadingSubtitle = "by Hydra#8270",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil,
+       FileName = "Big Hub"
+    },
+    Discord = {
+       Enabled = true,
+       Invite = "YvwEyH2W6t",
+       RememberJoins = true
+    },
+    KeySystem = false,
+    KeySettings = {
+       Title = "Sirius Hub",
+       Subtitle = "Key System",
+       Note = "Join the discord (discord.gg/sirius)",
+       FileName = "SiriusKey",
+       SaveKey = true,
+       GrabKeyFromSite = false, 
+       Key = "Hello"
+    }
+ })
+
+
+local Tab = Window:CreateTab("Main")
+
+local Slider = Tab:CreateSlider({
+   Name = "Coin Giver",
+   Info = "Give yourself coins (visuals only)",
+   Range = {0, 100000000},
+   Increment = 1,
+   Suffix = "Coins",
+   CurrentValue = game:GetService("Players").LocalPlayer.CoinAmount.Value.."",
+   Flag = "Slider1",
+   Callback = function(Value)
+        game:GetService("Players").LocalPlayer.CoinAmount.Value = Value
+   end,
+})
+
