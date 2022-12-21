c1124238d8add6d5a57fcdc4720b58057a7ced94
Bo <bo.van.laarhoven@gmail.com>
2022-12-21 20:42:21 +0100

    Update Silentaim.lua

--- Simulator/Silentaim.lua
+++ Simulator/Silentaim.lua
@@ -1,49 +1,62 @@
-local players = game:GetService("Players")
-local plr = players.LocalPlayer
+local plrs = game:GetService("Players")
+local plr = plrs.LocalPlayer
 local mouse = plr:GetMouse()
 local camera = game:GetService("Workspace").CurrentCamera
 
-function ClosestPlayerToMouse()
-    local dist = math.huge
+function notBehindWall(target)
+    local ray = Ray.new(plr.Character.Head.Position, (target.Position - plr.Character.Head.Position).Unit * 300)
+    local part, position = game:GetService("Workspace"):FindPartOnRayWithIgnoreList(ray, {plr.Character}, false, true)
+    if part then
+        local humanoid = part.Parent:FindFirstChildOfClass("Humanoid")
+        if not humanoid then
+            humanoid = part.Parent.Parent:FindFirstChildOfClass("Humanoid")
+        end
+        if humanoid and target and humanoid.Parent == target.Parent then
+            local pos, visible = camera:WorldToScreenPoint(target.Position)
+            if visible then
+                return true
+            end
+        end
+    end
+end
+ 
+function getPlayerClosestToMouse()
     local target = nil
-    for i,v in pairs(players:GetPlayers()) do
-        if v ~= plr and v.Character and v.Character:FindFirstChild("Head") then
-            local screenpoint = camera:WorldToScreenPoint(v.Character.Head.Position)
-            local check = (Vector2.new(mouse.X,mouse.Y)-Vector2.new(screenpoint.X,screenpoint.Y)).Magnitude
-            if check < dist then
-                check = dist
-                target = v
+    local maxDist = math.huge
+    for _,v in pairs(plrs:GetPlayers()) do
+        if v.Character then
+            if v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health ~= 0 and v.Character:FindFirstChild("HumanoidRootPart") and v.TeamColor ~= plr.TeamColor then
+                local pos, vis = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position)
+                local dist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(pos.X, pos.Y)).magnitude
+                if dist < maxDist and vis then
+                    local torsoPos = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position)
+                    local torsoDist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(torsoPos.X, torsoPos.Y)).magnitude
+                    local headPos = camera:WorldToViewportPoint(v.Character.Head.Position)
+                    local headDist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(headPos.X, headPos.Y)).magnitude
+                    if torsoDist > headDist then
+                        if notBehindWall(v.Character.Head) then
+                            target = v.Character.Head
+                        end
+                    else
+                        if notBehindWall(v.Character.HumanoidRootPart) then
+                            target = v.Character.HumanoidRootPart
+                        end
+                    end
+                    maxDist = dist
+                end
             end
         end
     end
     return target
 end
 
-local mt = getrawmetatable(game)
-setreadonly(mt,false)
-local namecall = mt.__namecall
-
-mt.__namecall = function(self,...)
-    local args = {...}
+local old
+old = hookmetamethod(game, "__namecall", function(self, ...)
     local method = getnamecallmethod()
-    if tostring(self) == "fire" and tostring(method) == "FireServer" then
-        args[1] = ClosestPlayerToMouse().Character.Head.Position
-        return self.FireServer(self,unpack(args))
-    end
-    return namecall(self,...)
-end
-
-local OldIndex = nil
-local OldNameCall = nil
-
-OldIndex = hookmetamethod(game, "__index", newcclosure(function(...)
-    local Self, Key = ...
-    return OldIndex(...)
-end))
-
-OldNameCall = hookmetamethod(game, "__namecall", newcclosure(function(...)
-    local Self = ...
-    local NamecallMethod = getnamecallmethod()
-
-    return OldNameCall(...)
-end))
+    local args = {...}
+    if tostring(self) == "fire" and method == "FireServer" then
+        args[2][1] = Vector3.new(getPlayerClosestToMouse().Character.Head.Position)
+        return old(self, unpack(args)) 
+    end 
+    return old(self, ...)
+end)
