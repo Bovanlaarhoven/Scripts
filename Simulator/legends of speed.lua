74df8ad9a52b888d9096eb48d9e7bf36762326ad
Bo <bo.van.laarhoven@gmail.com>
2022-12-26 10:51:53 +0100

    Update legends of speed.lua

--- Simulator/legends of speed.lua
+++ Simulator/legends of speed.lua
@@ -1,30 +1,53 @@
-getgenv().collectOrbsToggle = true
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
-spawn(function(AutoCollect)
-    local playerhead = game.Players.LocalPlayer.Character.Head
-    while wait() do
-    if not getgenv().collectOrbsToggle then break end
-        for i, v in pairs(game:GetService("Workspace").orbFolder:GetDescendants()) do
-            if v.Name == "TouchInterest" and v.Parent then
-                firetouchinterest(playerhead, v.Parent, 0)
-                wait(0.15)
-                break
+local Window = Rayfield:CreateWindow({
+	Name = "Legends of speed",
+	LoadingTitle = "Hydra Network v2",
+	LoadingSubtitle = "by hydra#8270",
+	ConfigurationSaving = {
+		Enabled = true,
+		FolderName = nil, 
+		FileName = "Big Hub"
+	},
+        Discord = {
+        	Enabled = true,
+        	Invite = "k9a4zym5uG", 
+        	RememberJoins = true 
+        },
+	KeySystem = false, 
+	KeySettings = {
+		Title = "Sirius Hub",
+		Subtitle = "Key System",
+		Note = "Join the discord (discord.gg/sirius)",
+		FileName = "SiriusKey",
+		SaveKey = true,
+		GrabKeyFromSite = false, 
+		Key = "Hello"
+	}
+})
+
+getgenv().AutoHoop = false
+
+task.spawn(function()
+    while task.wait(0) do
+        if getgenv().AutoHoop == true then
+            while true do task.wait()
+                for i,v in pairs(game:GetService("Workspace").Hoops:GetChildren()) do
+                    if v.Name == "Hoop" then
+                        v.CFrame = game:GetService("Players").LocalPlayer.Character.Head.CFrame
+                    end
+                end
             end
         end
     end
 end)
 
-spawn(function()
-    local playerhead = game.Players.LocalPlayer.Character.Head
-    while wait() do
-    if not getgenv().collectOrbsToggle then break end
-        for i, v in pairs(game:GetService("Workspace").Hoops:GetDescendants()) do
-            if v.Name == "TouchInterest" and v.Parent then
-                firetouchinterest(playerhead, v.Parent, 0)
-                wait(0.1)
-                firetouchinterest(playerhead, v.Parent, 1)
-                break
-            end
-        end
-    end
-end)
+local Tab = Window:CreateTab("Main", 4483362458)
+
+local Button = Tab:CreateButton({
+	Name = "Hoop Auto Farm",
+	Callback = function()
+		AutoHoop = true
+	end,
+})
