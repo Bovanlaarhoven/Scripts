56e87641a01ae783a46a2a80c1359806f56407c7
Bo <bo.van.laarhoven@gmail.com>
2022-12-27 18:29:44 +0100

    Create WhitelistChecker.lua

---
+++ WhitelistChecker.lua
@@ -0,0 +1,42 @@
+local filename = "Whitelist.txt"
+local json
+local HttpService = game:GetService("HttpService")
+local Http_Request = request;
+local Body = Http_Request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
+local Decoded = game:GetService('HttpService'):JSONDecode(Body)
+local GetHwid = Decoded.headers["Sw-Fingerprint"];
+local GAMENAME = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+local Time = os.clock()
+
+
+print("[Whitelist] Loading...")
+wait(0.2)
+print("[Whitelist] ".. identifyexecutor())
+wait(0.2)
+
+print("[Whitelist] Checking Whitelist...")
+
+if (readfile) then
+    json = HttpService:JSONEncode({["hwid"] = GetHwid})
+    readfile(filename, json)
+else
+    json = readfile(filename)
+end
+
+if (not json) then
+    print("[Whitelist] Failed to load whitelist.")
+    return
+end
+
+if (not HttpService:JSONDecode(json)) then
+    print("[Whitelist] Failed to load whitelist.")
+    return
+end
+
+if (HttpService:JSONDecode(json).hwid == GetHwid) then
+    print("[Whitelist] Whitelisted!")
+    return
+else
+    print("[Whitelist] Not Whitelisted!")
+    game:Shutdown()
+end
