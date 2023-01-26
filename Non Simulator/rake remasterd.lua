14728afaaf9f943afb7ebbf08f3e6ef86cb01af4
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-01-26 21:28:10 +0100

    Update rake remasterd.lua

--- Non Simulator/rake remasterd.lua
+++ Non Simulator/rake remasterd.lua
@@ -1,3 +1,4 @@
+local oldfog
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
 local Window = Rayfield:CreateWindow({
@@ -27,3 +28,26 @@ local Window = Rayfield:CreateWindow({
  })
  
 local Tab = Window:CreateTab("Main", 4483362458)
+
+local Toggle = Tab:CreateToggle({
+    Name = "No Fog",
+    CurrentValue = false,
+    Flag = "Toggle1",
+    Callback = function(Value)
+        if Value then
+            oldfog = game:GetService("ReplicatedStorage").CurrentLightingProperties.FogEnd.Value
+            game:GetService("ReplicatedStorage").CurrentLightingProperties.FogEnd.Value = math.huge
+        else
+            oldfog = game:GetService("ReplicatedStorage").CurrentLightingProperties.FogEnd.Value
+            game:GetService("ReplicatedStorage").CurrentLightingProperties.FogEnd.Value = oldfog
+        end
+    else
+        if oldfog then
+            if game:GetService("ReplicatedStorage").CurrentLightingProperties.FogEnd.Value then
+                game:GetService("ReplicatedStorage").CurrentLightingProperties.FogEnd.Value = oldfog
+            else
+                game:GetService("ReplicatedStorage").CurrentLightingProperties.FogEnd.Value = oldfog
+            end
+        end
+    end,
+ })
