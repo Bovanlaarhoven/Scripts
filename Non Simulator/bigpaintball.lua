5ba56ba2f8284db953ccad1aa443b25d396cc15e
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-01-28 22:51:16 +0100

    Create bigpaintball.lua

---
+++ Non Simulator/bigpaintball.lua
@@ -0,0 +1,7 @@
+for i,v in next, getgc(true) do
+    if type(v) == "table" and rawget(v, "damage") then
+        v.shotrate = 0.00001
+        v.velocity = 10000
+        v.additionalSpeed = 100
+    end
+end
