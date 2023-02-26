5fa2bd5ad82d325a42a3f9037241570e13d2e957
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-02-26 22:13:13 +0100

    Update bypass.lua

--- Non Simulator/bypass.lua
+++ Non Simulator/bypass.lua
@@ -6,16 +6,11 @@ for _, v in next, getconnections(game:GetService("ScriptContext").Error) do
     v:Disable()
 end
 
-local Log = game:GetService("ReplicatedStorage").ErrorLoggerRemote
-local ACDT = game:GetService("ReplicatedStorage").Communication.Events.ACDTResponse
+local AntiExploit = game:GetService("ReplicatedStorage").Remotes.AntiExploit
 local old
 old = hookmetamethod(game,"__namecall",function(self,...)
     local method = getnamecallmethod()
-    local args = {...}
-    if self == ACDT and method == "FireServer" then
-        args[1] = random
-    return old(self,unpack(args))
-    elseif self == Log and method == "FireServer" then
+    if self == AntiExploit and method == "FireServer" then
         return
     end
     return old(self,...)
