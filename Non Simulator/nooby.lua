2bc954ac3c3e601e14e2e4acb7c7796b80ff2214
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-07 00:01:41 +0100

    Update nooby.lua

--- Non Simulator/nooby.lua
+++ Non Simulator/nooby.lua
@@ -1,1 +1,11 @@
-print("test")
+local noob = true
+getgenv().esex = true
+_G.noob = true
+local Players = game:GetService('Players')
+local LocalPlayer = Players.LocalPlayer
+
+if not noob then
+    print("omg pro")
+else
+    print("omg noob")
+end
