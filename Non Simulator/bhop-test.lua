81c742af54c8d27c7811b9d3e929a37174c5c98f
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-11 01:03:50 +0100

    Update bhop-test.lua

--- Non Simulator/bhop-test.lua
+++ Non Simulator/bhop-test.lua
@@ -1,13 +1,12 @@
+
 getgenv().Bhop = true
-local Input = game:GetService("VirtualInputManager")
 local Humanoid = game:GetService("Players").LocalPlayer.Character.Humanoid
 local namecall
 
 namecall = hookmetamethod(game,"__namecall", function(self,...)
     local args = {...}
     if not checkcaller() and self and self.Name == "Communicator" and args[1] == "JumpDone" and getgenv().Bhop then
-        -- Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-        Input:SendKeyEvent(true ,Enum.KeyCode.Space ,false ,game)
+        Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
         return
     end
     return namecall(self,...)
