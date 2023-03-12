5a4a1488240db8b6e1ae3776c74377cb9b445e8f
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-12 17:45:47 +0100

    ok

--- Non Simulator/parkour.lua
+++ Non Simulator/parkour.lua
@@ -1,10 +1,18 @@
+for _,v in next, getconnections(game:GetService("ScriptContext").Error) do
+    v:Disable()
+end
+
+for _,v in next, getconnections(game:GetService("LogService").MessageOut) do
+    v:Disable()
+end
+
 local main = getupvalue(getsenv(game:GetService("Players").LocalPlayer.Backpack.Main).resetAmmo, 1)
 local getupvalue = (getupvalue or debug.getupvalue);
 local hookmetamethod = hookmetamethod or function(tbl, mt, func) return hookfunction(getrawmetatable(tbl)[mt], func) end;
 
 repeat wait() until game:IsLoaded();
 local players = game:GetService("Players");
-local client = players.LocalPlayer;
+local lplr = players.LocalPlayer;
 local variables, mainEnv, encrypt;
 local runservice = game:GetService("RunService");
 
@@ -25,51 +33,41 @@ do
         "GetCurrentCombo";
         "MaxCombo";
         "UpdateCombo";
-    };
+    }
 
-    local nc;
-    nc = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-        local args = {...};
-        local method = getnamecallmethod();
-    
+    local hook
+    hook = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+        local args = {...}
+        local method = getnamecallmethod()
+
         if (method == "FireServer" and table.find(banRemotes, self.Name)) then
-            return;
-        elseif (method == "TakeDamage" and self.ClassName == "Humanoid") then
-            return;
-        end;
-    
-        return nc(self, unpack(args));
-    end));
+            return
+        end
+        return hook(self, unpack(args))
+    end))
 
-    local idx;
-    idx = hookmetamethod(game, "__index", newcclosure(function(self, key)
+    local old
+    old = hookmetamethod(game, "__index", newcclosure(function(self, key)
         if (key == "PlaybackLoudness" and getfenv(2).script.Name == "RadioScript") then
             return 0;
-        end;
+        end
 
-        return idx(self, key);
-    end));
-
+        return old(self, key);
+    end))
+    
     local function onCharacterAdded(char)
-        if (not char) then return end;
-        wait(1);
-        local mainScript = client.Backpack:WaitForChild("Main");
-        variables = getupvalue(getsenv(mainScript).charJump, 1);
-        variables.adminLevel = 13;
-        getfenv().script = mainScript;
-        mainEnv = getsenv(mainScript);
-        encrypt = function(str)
-            local _, res = pcall(mainEnv.encrypt, str);
-            return res;
-        end;
-    end;
-    onCharacterAdded(client.Character);
-    client.CharacterAdded:Connect(onCharacterAdded);
-end;
-
-
-while task.wait() do
-    if main.comboLevel < 5 then
-        main.comboLevel = 5
+        if (not char)  then return end
+        wait(1)
+        local Main = lplr.Backpack:WaitForChild("Main")
+        variables = getupvalue(getsenv(Main).charJump, 1)
+        variables.adminLevel = 13
+        getfenv().script = Main
+        mainEnv = getsenv(Main)
+        encrypt = function(string)
+            local _, res = pcall(mainEnv.encrypt, string)
+            return res
+        end
     end
+    onCharacterAdded(lplr.Character);
+    lplr.CharacterAdded:Connect(onCharacterAdded);
 end
--- Non Simulator/test.lua
+++ Non Simulator/test.lua
@@ -4,7 +4,7 @@ local hookmetamethod = hookmetamethod or function(tbl, mt, func) return hookfunc
 
 repeat wait() until game:IsLoaded();
 local players = game:GetService("Players");
-local client = players.LocalPlayer;
+local lplr = players.LocalPlayer;
 local variables, mainEnv, encrypt;
 local runservice = game:GetService("RunService");
 
@@ -25,89 +25,41 @@ do
         "GetCurrentCombo";
         "MaxCombo";
         "UpdateCombo";
-    };
+    }
 
-    local nc;
-    nc = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-        local args = {...};
-        local method = getnamecallmethod();
-    
+    local hook
+    hook = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+        local args = {...}
+        local method = getnamecallmethod()
+
         if (method == "FireServer" and table.find(banRemotes, self.Name)) then
-            return;
-        elseif (method == "FireServer" and self.Name == "SubmitCombo" and args[1] > 299) then
-            args[1] = math.random(250, 299);
-        elseif (method == "TakeDamage" and self.ClassName == "Humanoid") then
-            return;
-        end;
-    
-        return nc(self, unpack(args));
-    end));
+            return
+        end
+        return hook(self, unpack(args))
+    end))
 
-    local idx;
-    idx = hookmetamethod(game, "__index", newcclosure(function(self, key)
+    local old
+    old = hookmetamethod(game, "__index", newcclosure(function(self, key)
         if (key == "PlaybackLoudness" and getfenv(2).script.Name == "RadioScript") then
             return 0;
-        end;
+        end
 
-        return idx(self, key);
-    end));
-
+        return old(self, key);
+    end))
+    
     local function onCharacterAdded(char)
-        if (not char) then return end;
-        wait(1);
-        local mainScript = client.Backpack:WaitForChild("Main");
-        variables = getupvalue(getsenv(mainScript).charJump, 1);
-        variables.adminLevel = 13;
-        getfenv().script = mainScript;
-        mainEnv = getsenv(mainScript);
-        encrypt = function(str)
-            local _, res = pcall(mainEnv.encrypt, str);
-            return res;
-        end;
-    end;
-
-    onCharacterAdded(client.Character);
-    client.CharacterAdded:Connect(onCharacterAdded);
-end;
-
-print("loaded")
-
-local moves = {
-    "slide";
-    "dropdown";
-    "ledgegrab";
-    "edgejump";
-    "longjump";
-    "vault";
-    "wallrun";
-    "springboard";
-  };
-  
-  runservice.RenderStepped:Connect(function()
-    if (client.Backpack and client.Backpack:FindFirstChild("Main") and client.PlayerScripts:FindFirstChild("Points") and getsenv(client.Backpack.Main)) then
-      local pointsEnv = getsenv(client.PlayerScripts.Points);
-      pointsEnv.changeParkourRemoteParent(workspace);
-    
-      local scoreRemote = getupvalue(pointsEnv.changeParkourRemoteParent, 2);
-    
-      scoreRemote:FireServer(encrypt("walljump"), {
-          [encrypt("walljumpDelta")] = encrypt(tostring(math.random(2.02, 3.55)));
-          [encrypt("combo")] = encrypt(tostring(math.random(4, 5)));
-      });
-    
-      wait(0.4);
-    
-      scoreRemote:FireServer(encrypt(moves[math.random(1, #moves)]), {
-          [encrypt("combo")] = encrypt(tostring(1));
-      });
-    
-      wait(math.random(1.25, 1.35));
-    end;
-    wait();
-  end)
-  
-while task.wait() do
-    if main.comboLevel < 5 then
-        main.comboLevel = 5
+        if (not char)  then return end
+        wait(1)
+        local Main = lplr.Backpack:WaitForChild("Main")
+        variables = getupvalue(getsenv(Main).charJump, 1)
+        variables.adminLevel = 13
+        getfenv().script = Main
+        mainEnv = getsenv(Main)
+        encrypt = function(string)
+            local _, res = pcall(mainEnv.encrypt, string)
+            return res
+        end
     end
+    onCharacterAdded(lplr.Character);
+    lplr.CharacterAdded:Connect(onCharacterAdded);
 end
