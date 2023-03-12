6fc719bc4c78656a58a11dfe5fe20b31f6eb4002
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-12 17:39:10 +0100

    Create parkour.lua

---
+++ Non Simulator/parkour.lua
@@ -0,0 +1,75 @@
+local main = getupvalue(getsenv(game:GetService("Players").LocalPlayer.Backpack.Main).resetAmmo, 1)
+local getupvalue = (getupvalue or debug.getupvalue);
+local hookmetamethod = hookmetamethod or function(tbl, mt, func) return hookfunction(getrawmetatable(tbl)[mt], func) end;
+
+repeat wait() until game:IsLoaded();
+local players = game:GetService("Players");
+local client = players.LocalPlayer;
+local variables, mainEnv, encrypt;
+local runservice = game:GetService("RunService");
+
+do
+    local banRemotes = {
+        "FireToDieInstantly";
+        "LoadString";
+        "FlyRequest";
+        "FinishTimeTrial";
+        "UpdateDunceList";
+        "FF";
+        "okbye";
+        "Fling";
+        "ClientFling";
+        "hl";
+        "LCombo";
+        "SubmitCombo";
+        "GetCurrentCombo";
+        "MaxCombo";
+        "UpdateCombo";
+    };
+
+    local nc;
+    nc = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+        local args = {...};
+        local method = getnamecallmethod();
+    
+        if (method == "FireServer" and table.find(banRemotes, self.Name)) then
+            return;
+        elseif (method == "TakeDamage" and self.ClassName == "Humanoid") then
+            return;
+        end;
+    
+        return nc(self, unpack(args));
+    end));
+
+    local idx;
+    idx = hookmetamethod(game, "__index", newcclosure(function(self, key)
+        if (key == "PlaybackLoudness" and getfenv(2).script.Name == "RadioScript") then
+            return 0;
+        end;
+
+        return idx(self, key);
+    end));
+
+    local function onCharacterAdded(char)
+        if (not char) then return end;
+        wait(1);
+        local mainScript = client.Backpack:WaitForChild("Main");
+        variables = getupvalue(getsenv(mainScript).charJump, 1);
+        variables.adminLevel = 13;
+        getfenv().script = mainScript;
+        mainEnv = getsenv(mainScript);
+        encrypt = function(str)
+            local _, res = pcall(mainEnv.encrypt, str);
+            return res;
+        end;
+    end;
+    onCharacterAdded(client.Character);
+    client.CharacterAdded:Connect(onCharacterAdded);
+end;
+
+
+while task.wait() do
+    if main.comboLevel < 5 then
+        main.comboLevel = 5
+    end
+end
