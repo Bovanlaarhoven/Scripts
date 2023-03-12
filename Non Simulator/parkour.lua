10e141d50362e2c973d9c9393799c14bc4b3260f
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-13 00:18:20 +0100

    Update parkour.lua

--- Non Simulator/parkour.lua
+++ Non Simulator/parkour.lua
@@ -283,16 +283,14 @@ local MyButton = RightGroupBox:AddButton('Autofarm', function()
         if (lplr.Backpack and lplr.Backpack:FindFirstChild("Main") and lplr.PlayerScripts:FindFirstChild("Points") and getsenv(lplr.Backpack.Main)) then
             local pointsEnv = getsenv(lplr.PlayerScripts.Points);
             pointsEnv.changeParkourRemoteParent(workspace);
-        
+
             local scoreRemote = getupvalue(pointsEnv.changeParkourRemoteParent, 2);
-        
+            
             scoreRemote:FireServer(encrypt("walljump"), {
                 [encrypt("walljumpDelta")] = encrypt(tostring(math.random(2.02, 3.55)));
                 [encrypt("combo")] = encrypt(tostring(math.random(4, 5)));
             });
-        
             wait(0.4);
-        
             scoreRemote:FireServer(encrypt(moves[math.random(1, #moves)]), {
                 [encrypt("combo")] = encrypt(tostring(1));
             });
