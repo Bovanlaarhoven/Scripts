12d15bf39483820a00f29dca70216039b123fe6c
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-12 15:02:49 +0100

    ok

--- Non Simulator/test.lua
+++ Non Simulator/test.lua
@@ -1,3 +1,9 @@
+local Settings = {
+    autofarm = false,
+    autocombo = false,
+}
+
+local main = getupvalue(getsenv(game:GetService("Players").LocalPlayer.Backpack.Main).resetAmmo, 1)
 local getupvalue = (getupvalue or debug.getupvalue);
 local hookmetamethod = hookmetamethod or function(tbl, mt, func) return hookfunction(getrawmetatable(tbl)[mt], func) end;
 
@@ -82,25 +88,39 @@ local moves = {
   "springboard";
 };
 
-runservice.RenderStepped:Connect(function()
-  if (client.Backpack and client.Backpack:FindFirstChild("Main") and client.PlayerScripts:FindFirstChild("Points") and getsenv(client.Backpack.Main)) then
-    local pointsEnv = getsenv(client.PlayerScripts.Points);
-    pointsEnv.changeParkourRemoteParent(workspace);
-  
-    local scoreRemote = getupvalue(pointsEnv.changeParkourRemoteParent, 2);
-  
-    scoreRemote:FireServer(encrypt("walljump"), {
-        [encrypt("walljumpDelta")] = encrypt(tostring(math.random(2.02, 3.55)));
-        [encrypt("combo")] = encrypt(tostring(math.random(4, 5)));
-    });
-  
-    wait(0.4);
-  
-    scoreRemote:FireServer(encrypt(moves[math.random(1, #moves)]), {
-        [encrypt("combo")] = encrypt(tostring(1));
-    });
-  
-    wait(math.random(1.25, 1.35));
-  end;
-  wait();
+task.spawn(function()
+	while task.wait() do
+		if Settings.autocombo then
+            if main.comboLevel < 5 then
+                main.comboLevel = 5
+            end
+		end
+	end
+end)
+
+task.spawn(function()
+    if Settings.autofarm then
+    runservice.RenderStepped:Connect(function()
+        if (client.Backpack and client.Backpack:FindFirstChild("Main") and client.PlayerScripts:FindFirstChild("Points") and getsenv(client.Backpack.Main)) then
+            local pointsEnv = getsenv(client.PlayerScripts.Points);
+            pointsEnv.changeParkourRemoteParent(workspace);
+        
+            local scoreRemote = getupvalue(pointsEnv.changeParkourRemoteParent, 2);
+        
+            scoreRemote:FireServer(encrypt("walljump"), {
+                [encrypt("walljumpDelta")] = encrypt(tostring(math.random(2.02, 3.55)));
+                [encrypt("combo")] = encrypt(tostring(math.random(4, 5)));
+            });
+        
+            wait(0.4);
+        
+            scoreRemote:FireServer(encrypt(moves[math.random(1, #moves)]), {
+                [encrypt("combo")] = encrypt(tostring(1));
+            });
+        
+            wait(math.random(1.25, 1.35));
+        end;
+        wait();
+        end)
+    end
 end)
