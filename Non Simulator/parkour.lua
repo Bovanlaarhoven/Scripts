4a632a0b8eb3820428dd492cdeaaf38a97ac26e7
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-17 19:09:57 +0100

    Update parkour.lua

--- Non Simulator/parkour.lua
+++ Non Simulator/parkour.lua
@@ -733,22 +733,20 @@ end)
 
 local MyButton = Other:AddButton('Join Discord', function()
     Request({
-       Url = "http://127.0.0.1:6463/rpc?v=1",
-       Method = "POST",
-       Headers = {
-           ["Content-Type"] = "application/json",
-           ["origin"] = "https://discord.com",
-       },
-       Body = game:GetService("HttpService"):JSONEncode(
-           {
-               ["args"] = {
-                   ["code"] = "YvwEyH2W6t",
-               },
-               ["cmd"] = "/invite/YvwEyH2W6t",
-               ["nonce"] = "."
-           })
-   })
-   print("noob")
+        Url = "http://127.0.0.1:6463/rpc?v=1",
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json",
+            ["Origin"] = "https://discord.com"
+        },
+        Body = game:GetService("HttpService"):JSONEncode({
+            cmd = "INVITE_BROWSER",
+            args = {
+                code = "YvwEyH2W6t"
+            },
+            nonce = game:GetService("HttpService"):GenerateGUID(false)
+        }),
+    })
 end)
 
 --Visuals Tab--
