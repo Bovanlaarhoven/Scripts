0fd5e1f63135d974844496ce277b3aedb3a9c403
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-23 19:21:39 +0100

    Update parkour.lua

--- Non Simulator/parkour.lua
+++ Non Simulator/parkour.lua
@@ -749,27 +749,6 @@ local MyButton = Other:AddButton('Unlock Badges', function()
         firetouchinterest(lplr.Character.HumanoidRootPart, part, 0);
     end;
 end)
-
-local MyButton = Other:AddButton('Join Discord', function()
-    syn.request(
-        {
-            Url = "http://127.0.0.1:6463/rpc?v=1",
-            Method = "POST",
-            Headers = {
-                ["Content-Type"] = "application/json",
-                ["origin"] = "https://discord.com",
-            },
-            Body = game:GetService("HttpService"):JSONEncode(
-                {
-                    ["args"] = {
-                        ["code"] = "YvwEyH2W6t",
-                    },
-                    ["cmd"] = "INVITE_BROWSER",
-                    ["nonce"] = "."
-                })
-        })
-end)
-
 --Visuals Tab--
 
 Visuals:AddToggle('Esp', {
