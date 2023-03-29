4efb5dc05a73bcb79b44d017a1305b13661f84dc
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-30 01:07:18 +0200

    Update noob.lua

---
+++ Non Simulator/noob.lua
@@ -0,0 +1,5 @@
+local players = game:GetService("Players")
+local lplr = players.LocalPlayer
+local char = lplr.Character
+local Camera = game:Getservice("Workspace").CurrentCamera
+local WorldToViewportPoint = Camera.WorldToViewportPoint
