14438dccca9072abd3616f4fda01364870441486
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-06 14:08:26 +0200

    Update esp.lua

--- Non Simulator/esp.lua
+++ Non Simulator/esp.lua
@@ -1,7 +1,13 @@
+local settings = {
+    boxesp = true,
+    team = false,
+}
+
 local plrs = game:GetService("Players")
 local lplr = plrs.LocalPlayer
 local CurrentCamera = workspace.CurrentCamera
 local WorldToViewportPoint = CurrentCamera.WorldToViewportPoint
+
 
 local headoff = Vector3.new(0, 0.5, 0)
 local legoff = Vector3.new(0, 3, 0)
@@ -11,20 +17,29 @@ for i,v in pairs(plrs:GetChildren()) do
     local box = Drawing.new("Square")
 
     boxoutline.Visible = false
-    boxoutline.Color = Color3.fromRGB(255, 255, 255)
+    boxoutline.Color = Color3.fromRGB(0, 0, 0)
     boxoutline.Thickness = 2
-    boxoutline.Transparency = 1
+    boxoutline.Transparency = 0
     boxoutline.Filled = false
 
     box.Visible = false
-    box.Color = Color3.fromRGB(255, 255, 255)
+    box.Color = Color3.fromRGB(43, 42, 42)
     box.Thickness = 1
-    box.transparency = 1
+    box.transparency = 0
     box.Filled = false
     
     function boxesp()
         game:GetService("RunService").RenderStepped:Connect(function()
-            if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 then
+            if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 and settings.boxesp == true then
+                if settings.team == true then
+                    if plrs.Character.Humanoid.Team == lplr.Character.Humanoid.Team then
+                        boxoutline.Color = Color3.fromRGB(0, 255, 0)
+                        box.Color = Color3.fromRGB(0, 255, 0)
+                    else
+                        boxoutline.Color = Color3.fromRGB(255, 0, 0)
+                        box.Color = Color3.fromRGB(255, 0, 0)
+                    end                    
+                end
                 local vector, onScreen = WorldToViewportPoint(CurrentCamera, v.Character.HumanoidRootPart.Position)
                 local RootPart = v.Character.HumanoidRootPart
                 local Head = v.Character.Head
@@ -70,7 +85,7 @@ plrs.PlayerAdded:Connect(function(v)
     
     function boxesp()
         game:GetService("RunService").RenderStepped:Connect(function()
-            if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 then
+            if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 and settings.boxesp == true then
                 local vector, onScreen = WorldToViewportPoint(CurrentCamera, v.Character.HumanoidRootPart.Position)
                 local RootPart = v.Character.HumanoidRootPart
                 local Head = v.Character.Head
