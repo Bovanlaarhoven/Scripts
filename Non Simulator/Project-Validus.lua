aceeaed34c2d8c020cadf0d61d721351b95d8cb3
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-06 22:38:31 +0200

    Create Project-Validus.lua

---
+++ Non Simulator/Project-Validus.lua
@@ -0,0 +1,106 @@
+local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+local plrs = game:GetService('Players')
+local lplr = plrs.LocalPlayer
+local mouse = lplr:GetMouse()
+local camera = workspace.CurrentCamera
+local WorldToViewportPoint = camera.WorldToViewportPoint
+local Fov = Drawing.new("Circle")
+Fov.Radius = 50
+Fov.Color = Color3.fromRGB(255, 255, 255)
+Fov.Filled = false
+Fov.NumSides = 32 
+Fov.Position = Vector2.new(20, 20)
+Fov.Transparency = 0.1
+
+local Window = Library:CreateWindow({
+    Title = 'Project-Validus',
+    Center = false,
+    AutoShow = true,
+    TabPadding = 4,
+})
+
+local Tabs = {
+    Main = Window:AddTab('Main'),
+    ['UI Settings'] = Window:AddTab('UI Settings'),
+}
+
+local tab1 = Tabs.Main:AddLeftTabbox()
+local AimAssistSetting = tab1:AddTab('Aim Assist')
+local FovSetting = tab1:AddTab('Fov Settings')
+
+FovSetting:AddToggle('FovVisable', {
+    Text = 'Fov Visable',
+    Default = false,
+    Tooltip = 'changes the fov visability',
+})
+
+Toggles.FovVisable:OnChanged(function()
+    Fov.Visible = Toggles.FovVisable.Value
+end)
+
+FovSetting:AddSlider('FovTransparency', {
+    Text = 'Fov Transparency',
+    Default = 1,
+    Min = 0,
+    Max = 1,
+    Rounding = 1,
+    Compact = true,
+})
+
+Options.FovTransparency:OnChanged(function()
+    Fov.Transparency = Options.FovTransparency.Value
+end)
+
+
+--ui settings
+Library:SetWatermark('Project-Validus By Hydra#8270')
+
+Library:OnUnload(function()
+    Library.Unloaded = true
+end)
+
+local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
+local Other = Tabs['UI Settings']:AddRightGroupbox('Other')
+
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+
+MenuGroup:AddToggle('keybindframe', {
+    Text = 'Keybind Frame',
+    Default = false,
+    Tooltip = 'Toggles KeybindFrame',
+})
+
+Toggles.keybindframe:OnChanged(function()
+    Library.KeybindFrame.Visible = Toggles.keybindframe.Value
+end)
+
+MenuGroup:AddToggle('Watermark', {
+    Text = 'Watermark',
+    Default = false,
+    Tooltip = 'Toggles Watermark',
+})
+
+Other:AddDropdown('MyPlayerDropdown', {
+    SpecialType = 'Player',
+    Text = 'A player dropdown',
+    Tooltip = 'Player Dropdown',
+})
+
+Toggles.Watermark:OnChanged(function()
+    Library:SetWatermarkVisibility(Toggles.Watermark.Value)
+end)
+
+Library.ToggleKeybind = Options.MenuKeybind
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+SaveManager:IgnoreThemeSettings() 
+SaveManager:SetIgnoreIndexes({ 'MenuKeybind' }) 
+ThemeManager:SetFolder('MyScriptHub')
+SaveManager:SetFolder('MyScriptHub/specific-game')
+SaveManager:BuildConfigSection(Tabs['UI Settings']) 
+ThemeManager:ApplyToTab(Tabs['UI Settings'])
