4564a570d7bc840f7d59ae692f3897fa225775fd
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-06 14:26:45 +0200

    Update esp.lua

--- Non Simulator/esp.lua
+++ Non Simulator/esp.lua
@@ -12,23 +12,30 @@ local headoff = Vector3.new(0, 0.5, 0)
 local legoff = Vector3.new(0, 3, 0)
 
 
-for _,v in pairs(plrs:GetChildren()) do
-    local boxoutline = Drawing.new("Square")
-    local box = Drawing.new("Square")
+local function boxesp()
+    for _,v in pairs(plrs:GetChildren()) do
+        local boxoutline = Drawing.new("Square")
+        local box = Drawing.new("Square")
+        local text = Drawing.new("Text")
 
-    boxoutline.Visible = false
-    boxoutline.Color = Color3.fromRGB(0, 0, 0)
-    boxoutline.Thickness = 2
-    boxoutline.Transparency = 0
-    boxoutline.Filled = false
+        boxoutline.Visible = false
+        boxoutline.Color = Color3.fromRGB(0, 0, 0)
+        boxoutline.Thickness = 2
+        boxoutline.Transparency = 0
+        boxoutline.Filled = false
 
-    box.Visible = false
-    box.Color = Color3.fromRGB(43, 42, 42)
-    box.Thickness = 1
-    box.transparency = 0
-    box.Filled = false
-    function boxesp()
-        game:GetService("RunService").RenderStepped:Connect(function()
+        box.Visible = false
+        box.Color = Color3.fromRGB(43, 42, 42)
+        box.Thickness = 1
+        box.Transparency = 0
+        box.Filled = false
+
+        text.Visible = false
+        text.Color = Color3.fromRGB(255, 255, 255)
+        text.Size = 15
+        text.Center = true
+
+        function updateBox()
             if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 and settings.boxesp == true then
                 if settings.teamcheck == true then
                     if v.Team == lteam then
@@ -37,14 +44,14 @@ for _,v in pairs(plrs:GetChildren()) do
                     else
                         boxoutline.Color = Color3.fromRGB(255, 0, 0)
                         box.Color = Color3.fromRGB(255, 0, 0)
-                    end                    
+                    end
                 end
                 local vector, onScreen = WorldToViewportPoint(CurrentCamera, v.Character.HumanoidRootPart.Position)
                 local RootPart = v.Character.HumanoidRootPart
                 local Head = v.Character.Head
                 local RootPosition, RootVis = WorldToViewportPoint(CurrentCamera, RootPart.Position)
                 local HeadPosition = WorldToViewportPoint(CurrentCamera, Head.Position + headoff)
-                local LegPostion = WorldToViewportPoint(CurrentCamera, RootPart.Position - legoff) 
+                local LegPostion = WorldToViewportPoint(CurrentCamera, RootPart.Position - legoff)
                 if onScreen then
                     boxoutline.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPostion.Y)
                     boxoutline.Position = Vector2.new(RootPosition.X - boxoutline.Size.X / 2, RootPosition.Y - boxoutline.Size.Y / 2)
@@ -53,22 +60,31 @@ for _,v in pairs(plrs:GetChildren()) do
                     box.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPostion.Y)
                     box.Position = Vector2.new(RootPosition.X - box.Size.X / 2, RootPosition.Y - box.Size.Y / 2)
                     box.Visible = true
+
+                    text.Position = boxoutline.Position - Vector2.new(0, 20)
+                    text.Text = v.Name
+                    text.Visible = true
                 else
                     boxoutline.Visible = false
                     box.Visible = false
+                    text.Visible = false
                 end
             else
                 boxoutline.Visible = false
                 box.Visible = false
+                text.Visible = false
             end
-        end)
+        end
+
+        coroutine.wrap(updateBox)()
     end
-    coroutine.wrap(boxesp)()
 end
+  
 
 plrs.PlayerAdded:Connect(function(v)
     local boxoutline = Drawing.new("Square")
     local box = Drawing.new("Square")
+    local text = Drawing.new("Text")
 
     boxoutline.Visible = false
     boxoutline.Color = Color3.fromRGB(0, 0, 0)
@@ -92,12 +108,6 @@ plrs.PlayerAdded:Connect(function(v)
                         boxoutline.Color = Color3.fromRGB(255, 0, 0)
                         box.Color = Color3.fromRGB(255, 0, 0)
                     end                    
-                end
-                if settings.teamhide == true then
-                    if v.Team == lteam then
-                        boxoutline.Visible = false
-                        box.Visible = false
-                    end
                 end
                 local vector, onScreen = WorldToViewportPoint(CurrentCamera, v.Character.HumanoidRootPart.Position)
                 local RootPart = v.Character.HumanoidRootPart
