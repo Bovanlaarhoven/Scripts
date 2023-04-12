7d282fe5dce7600f6bfbd90579a49317efd0d492
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-12 18:46:35 +0200

    Update Project-Validus.lua

--- Non Simulator/Project-Validus.lua
+++ Non Simulator/Project-Validus.lua
@@ -926,32 +926,28 @@ end)
 local MyButton = MenuGroup:AddButton({
     Text = 'Join Discord',
     Func = function()
-        Library:Unload()
+        Request(
+            {
+                Url = "http://127.0.0.1:6463/rpc?v=1",
+                Method = "POST",
+                Headers = {
+                    ["Content-Type"] = "application/json",
+                    ["origin"] = "https://discord.com",
+                },
+                Body = game:GetService("HttpService"):JSONEncode(
+                    {
+                        ["args"] = {
+                            ["code"] = "YvwEyH2W6t",
+                        },
+                        ["cmd"] = "INVITE_BROWSER",
+                        ["nonce"] = "."
+                    })
+            })
     end,
     DoubleClick = false,
-    Tooltip = 'Unload Script'
+    Tooltip = 'makes you join the discord server'
 })
 
-
-local MyButton = MenuGroup:AddButton('Join Discord', function()
-    Request(
-        {
-            Url = "http://127.0.0.1:6463/rpc?v=1",
-            Method = "POST",
-            Headers = {
-                ["Content-Type"] = "application/json",
-                ["origin"] = "https://discord.com",
-            },
-            Body = game:GetService("HttpService"):JSONEncode(
-                {
-                    ["args"] = {
-                        ["code"] = "YvwEyH2W6T",
-                    },
-                    ["cmd"] = "INVITE_BROWSER",
-                    ["nonce"] = "."
-                })
-        })
-end)
 
 
 Library.ToggleKeybind = Options.MenuKeybind
