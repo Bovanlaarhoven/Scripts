313438e1460950b741e9b1e38215fc6ded80a6c7
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-12 02:19:09 +0200

    oki

--- whitelist/oki2.lua
+++ whitelist/oki2.lua
@@ -1,31 +1,35 @@
 _G.Key = "my[key1]"
 
-local startTime = os.clock()
-print("Starting whitelist check...")
 local Request = (syn and syn.request or request or http and http.request or http_request) or error("No request function found")
-local saves = loadstring(game:HttpGet("https://pastebin.com/raw/yH20wkeZ"))()
+local encryptedKeys = loadstring(game:HttpGet("https://pastebin.com/raw/yH20wkeZ"))()
 local body = Request({Url = "https://httpbin.org/get", Method = "GET"}).Body
 local decode = game:GetService("HttpService"):JSONDecode(body)
 
-local keyMatch = false
-for _, validKey in ipairs(saves) do
-   local keyBytes = {}
-   for i = 1, #validKey, 2 do
-      local byte = tonumber(validKey:sub(i, i + 1), 16)
-      table.insert(keyBytes, byte)
+-- Decrypt the keys and store them in the saves table
+local saves = {}
+for i, encryptedKey in ipairs(encryptedKeys) do
+   local decryptedKey = ""
+   for j = 1, #encryptedKey, 2 do
+      local byte = tonumber(encryptedKey:sub(j, j+1), 16)
+      local keyByte = string.byte(encryptionKey, (j/2) % #encryptionKey + 1)
+      local decryptedByte = bit32.bxor(byte, keyByte)
+      decryptedKey = decryptedKey .. string.char(decryptedByte)
    end
-   local keyString = string.char(unpack(keyBytes))
-   if _G.Key == keyString then
+   saves[i] = decryptedKey
+end
+
+-- Check if the provided key matches one of the saved keys
+local keyMatch = false
+for _, savedKey in ipairs(saves) do
+   if _G.Key == savedKey then
       keyMatch = true
       break
    end
 end
 
+-- Print the result
 if keyMatch then
    print("Whitelisted")
 else
    warn("Invalid key provided. Loadstring not executed.")
 end
-
-local elapsedTime = os.clock() - startTime
-print("Elapsed time: " .. elapsedTime .. " seconds")
