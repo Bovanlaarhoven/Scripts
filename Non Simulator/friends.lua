c67c4f9e620adb4e811e176c5dd905df2f9bf31e
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-16 20:02:39 +0200

    Update friends.lua

--- Non Simulator/friends.lua
+++ Non Simulator/friends.lua
@@ -1,5 +1,6 @@
 local rootpart = game.Players.LocalPlayer.Character.HumanoidRootPart
 local workspace = workspace:GetChildren()
+local group = workspace.GroupBuildStructures:GetChildren()
 
 while (task.wait(0.1)) do
     for i = 1, #workspace do
@@ -9,10 +10,9 @@ while (task.wait(0.1)) do
             firetouchinterest(rootpart, v.TouchTrigger, 1)
         end
     end
-    
-    for i = 1, #workspace do
-        local v = workspace[i];
-        if v.ignore:GetChildren():FindFirstChild("Trigger") then
+    for i = 1, #group do
+        local v = group[i];
+        if v:FindFirstChild("Trigger") then
             firetouchinterest(rootpart, v.Trigger, 0)
             firetouchinterest(rootpart, v.Trigger, 1)
         end
