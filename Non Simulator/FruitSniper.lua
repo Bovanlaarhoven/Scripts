aa9c8f9f543c7a363f942c1531fdbd5a1d020d59
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-25 16:26:28 +0200

    Update FruitSniper.lua

--- Non Simulator/FruitSniper.lua
+++ Non Simulator/FruitSniper.lua
@@ -1,5 +1,12 @@
+repeat wait() until game:IsLoaded()
+mousemoveabs(706, 531)
+mouse1click()
+
 local lplr = game:GetService("Players").LocalPlayer
 local lcharacter = lplr.Character
+local TweenService = game:GetService("TweenService")
+local TweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0)
+local FruitName = "nil"
 
 local Fruits = {
     "Kilo Fruit",
@@ -39,38 +46,90 @@ local Fruits = {
     "Leopard Fruit",
 }
 
-local function getfruit()
-    local FruitFound = false
-    for _,v in pairs(Fruits) do
-        if game:GetService("Workspace"):FindFirstChild(v) then
-            print("Fruit Found: " .. v)
-            FruitFound = true
-        end
-    end
-    print("Function called. FruitFound = " .. tostring(FruitFound))
-    return FruitFound
-end
-
-local function NotFound(FruitFound)
-    if FruitFound == false then
-        print("no fruits... hopping")
-    end
-end
+local StoreNames = {
+    Fruits[1] == "Kilo-Kilo",
+    Fruits[2] == "Spin-Spin",
+    Fruits[3] == "Chop-Chop",
+    Fruits[4] == "Spring-Spring",
+    Fruits[5] == "Bomb-Bomb",
+    Fruits[6] == "Smoke-Smoke",
+    Fruits[7] == "Spike-Spike",
+    Fruits[8] == "Flame-Flame",
+    Fruits[9] == "Falcon-Falcon",
+    Fruits[10] == "Ice-Ice",
+    Fruits[11] == "Sand-Sand",
+    Fruits[12] == "Dark-Dark",
+    Fruits[13] == "Revive-Revive",
+    Fruits[14] == "Diamond-Diamond",
+    Fruits[15] == "Light-Light",
+    Fruits[16] == "Rubber-Rubber",
+    Fruits[17] == "Barrier-Barrier",
+    Fruits[18] == "Magma-Magma",
+    Fruits[19] == "Quake-Quake",
+    Fruits[20] == "Buddha-Buddha",
+    Fruits[21] == "Love-Love",
+    Fruits[22] == "Spider-Spider",
+    Fruits[23] == "Pheonix-Pheonix",
+    Fruits[24] == "Portal-Portal",
+    Fruits[25] == "Rumble-Rumble",
+    Fruits[26] == "Paw-Paw",
+    Fruits[27] == "Blizzard-Blizzard",
+    Fruits[28] == "Gravity-Gravity",
+    Fruits[29] == "Dough-Dough",
+    Fruits[30] == "Shadow-Shadow",
+    Fruits[31] == "Venom-Venom",
+    Fruits[32] == "Control-Control",
+    Fruits[33] == "Spirit-Spirit",
+    Fruits[34] == "Dragon-Dragon",
+    Fruits[35] == "Leopard-Leopard",
+}
 
 local function hopServer()
     local teleportService = game:GetService("TeleportService")
     local success, errorMessage = pcall(function()
         teleportService:Teleport(game.PlaceId)
     end)
-    
+
     if not success then
         warn("Failed to hop servers: " .. errorMessage)
     end
 end
 
-local fruitFound = getfruit()
-
-if NotFound(fruitFound) then
-    hopServer()
+local function NotFound(FruitFound)
+    if not FruitFound then
+        print("Fruit not found. Hopping servers.")
+        hopServer()
+    end
 end
+
+local function findfruit()
+    local FruitFound = false
+    for _,v in pairs(Fruits) do
+        if game:GetService("Workspace"):FindFirstChild(v) then
+            print("Fruit Found: " .. v)
+            FruitName = v
+            FruitFound = true
+            local tween = TweenService:Create(lcharacter.HumanoidRootPart, TweenInfo, {CFrame = game:GetService("Workspace")[v].Fruit.CFrame})
+            tween:Play()
+        end
+    end
+    print("Function called. FruitFound = " .. tostring(FruitFound))
+    NotFound(FruitFound)
+    return FruitFound
+end
+
+local function Store()
+    local args1 = "StoreFruit"
+    local args2 = "Kilo-Kilo"
+    local args3 = workspace.Characters[lplr.Name][FruitName]
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(args1, args2, args3)
+end
+
+
+findfruit()
+
+
+
+
+
 
