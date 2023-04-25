cbbd90f24e885ae6008b6fb033de084b7e037c8a
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-25 12:06:38 +0200

    ok

--- Non Simulator/FocatTheNoob.lua
+++ Non Simulator/FocatTheNoob.lua
@@ -1,37 +1,5 @@
-local getupvalue = (getupvalue or debug.getupvalue)
-local hookmetamethod = hookmetamethod or function(tbl, mt, func) return hookfunction(getrawmetatable(tbl)[mt], func) end
-local Request = (syn and syn.request or request or http and http.request or http_request) or error("No request function")
-local Tablefind = table.find
-local players = game:GetService("Players")
-local lplr = players.LocalPlayer
-local camera = workspace.CurrentCamera
-local runservice = game:GetService("RunService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local MainEvent = ReplicatedStorage.MainEvent
-
-local Remotes = {
-   "CHECKER_1",
-   "TeleportDetect",
-   "OneMoreTime"
-}
-
-local namecall
-namecall = hookmetamethod(game, "__namecall", function(...)
-   local args = {...}
-   local method = getnamecallmethod()
-   local self = args[1]
-   if not checkcaller() and (method == "FireServer" and self == MainEvent and Tablefind(Remotes, args[2])) then
-      return
-   end
-   return namecall(...)
-end)
-
-local __newindex
-__newindex = hookmetamethod(game, "__newindex", function(t, k, v)
-    if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
-        return
-    end
-    return __newindex(t, k, v)
+repeat wait() until game:IsLoaded() wait()
+game:GetService("Players").LocalPlayer.Idled:connect(function()
 end)
 
 local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
@@ -41,11 +9,12 @@ local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
 local Window = Library:CreateWindow({
-    Title = 'Script by Hydra#8270',
+    Title = "HydraWare",
     Center = true,
     AutoShow = true,
     TabPadding = 4
 })
+
 
 local Tabs = {
    Legit = Window:AddTab('Legit'),
@@ -53,16 +22,15 @@ local Tabs = {
    Players = Window:AddTab('Players'),
    Visuals = Window:AddTab('Visuals'),
    Misc = Window:AddTab('Misc'),
-   Settings = Window:AddTab('Settings'),
-   Config = Window:AddTab('Config'),
    ['UI Settings'] = Window:AddTab('UI Settings'),
 }
+
 
 local Aiming = Tabs.Legit:AddLeftGroupbox('Aiming')
 local AimVisuals = Tabs.Legit:AddLeftGroupbox('Aim Visuals')
 
 
-Library:SetWatermark('Script By Hydra#8270')
+Library:SetWatermark('HydraWare By Hydra#8270')
 
 Library:OnUnload(function()
     Library.Unloaded = true
