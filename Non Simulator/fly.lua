14acd0988880c47e6f190b27f6f5d6b30543fcd0
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-05-03 23:44:51 +0200

    Update fly.lua

--- Non Simulator/fly.lua
+++ Non Simulator/fly.lua
@@ -4,6 +4,48 @@ local uis = game:GetService("UserInputService")
 
 local canMove = true
 local playerVelocity = char.HumanoidRootPart.Velocity
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Fly Script",
+    LoadingTitle = "Loading script duhh",
+    LoadingSubtitle = "by Hydra#8270",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil,
+       FileName = "Big Hub"
+    },
+    Discord = {
+       Enabled = false,
+       Invite = "noinvitelink",
+       RememberJoins = true
+    },
+    KeySystem = false,
+    KeySettings = {
+       Title = "Untitled",
+       Subtitle = "Key System",
+       Note = "No method of obtaining the key is provided",
+       FileName = "Key",
+       SaveKey = true,
+       GrabKeyFromSite = false,
+       Key = {"Hello"}
+    }
+})
+
+local Tab = Window:CreateTab("Main", 4483362458)
+
+local Toggle = Tab:CreateToggle({
+    Name = "Fly",
+    CurrentValue = false,
+    Flag = "Toggle1",
+    Callback = function(Value)
+        canMove = Value
+        if not canMove then
+            playerVelocity = Vector3.new(0, 0, 0)
+            char.HumanoidRootPart.Velocity = playerVelocity
+        end
+    end,
+})
 
 uis.InputBegan:Connect(function(input, gameProcessed)
     if not gameProcessed and input.KeyCode == Enum.KeyCode.Q then
