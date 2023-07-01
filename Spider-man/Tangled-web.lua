f6a1c167f6e7ffeaa7d53b6c4216e5572cb6e446
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-07-01 16:19:11 +0200

    Update Tangled-web.lua

--- Spider-man/Tangled-web.lua
+++ Spider-man/Tangled-web.lua
@@ -1,20 +1,8 @@
 local plr = game:GetService("Players").LocalPlayer
 local RootPart = plr.Character.HumanoidRootPart
-getgenv().AutoCriminal = true
 
-local Attack = function()
-    game:GetService("ReplicatedStorage").RemoteEvent:FireServer("Combat", "Punch", false)
-end
-
-task.defer(function()
-    while task.wait() do
-        if AutoCriminal then
-            for _,v in pairs(workspace.Enemies:GetDescendants()) do
-                if v.Name == "HumanoidRootPart" then
-                    RootPart.CFrame = v.CFrame + Vector3.new(0, 2.5, 0)
-                    Attack()
-                end
-            end
-        end
+for _,v in pairs(game:GetService("Workspace").CollectableMoney:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+        firetouchinterest(RootPart, v.Parent, 1)
     end
-end)
+end
