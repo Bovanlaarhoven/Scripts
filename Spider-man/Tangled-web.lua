bf2bd7bcbf395f4480dc912c3e3bc11d261086fe
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-07-08 23:12:04 +0200

    Update Tangled-web.lua

--- Spider-man/Tangled-web.lua
+++ Spider-man/Tangled-web.lua
@@ -1,8 +1,18 @@
-local plr = game:GetService("Players").LocalPlayer
-local RootPart = plr.Character.HumanoidRootPart
+local plr = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
 
-for _,v in pairs(game:GetService("Workspace").CollectableMoney:GetDescendants()) do
-    if v.Name == "TouchInterest" then
-        firetouchinterest(RootPart, v.Parent, 1)
+local quest = function()
+    game:GetService("ReplicatedStorage").RemoteEvent:FireServer( "Dialogue", "GiveMission", workspace["Pizza Chef"])
+end
+
+while true do
+    quest()
+    wait(1)
+    for _,v in pairs(workspace.PizzaParts:GetDescendants()) do
+        if v.Name == "Head" then
+            for i = 1,4 do
+                plr.CFrame = v.CFrame + Vector3.new(0, 5, 0)
+                wait(.1)
+            end
+        end
     end
 end
