5e336ec3f86a9b7e0943a30cefcc269e20bcd206
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-07-14 01:29:04 +0200

    Update Bloxfruits.lua

--- Non Simulator/Bloxfruits.lua
+++ Non Simulator/Bloxfruits.lua
@@ -6,17 +6,27 @@ local tools = {}
 local Enemy = workspace.Enemies:GetChildren()
 local Mouse = plr:GetMouse()
 
+local Quests = {
+    "BanditQuest1",
+    "JungleQuest1",
+}
+
 for _, v in pairs(plr.Backpack:GetChildren()) do
     if v:IsA("Tool") then
         table.insert(tools, v.Name)
     end
 end
 
+local Quest = function()
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(1, "BanditQuest1", "StartQuest")
+end
+
 local getClosest = function()
     local closest = nil
     local dist = math.huge
     for _, v in pairs(Enemy) do
-        if v:FindFirstChild("HumanoidRootPart") and v:FindFirstChild("Humanoid") and v.Humanoid.Health > 0 then
+        local enemyName = v.Name
+        if not string.find(enemyName, "%[Boss%]") and v:FindFirstChild("HumanoidRootPart") and v:FindFirstChild("Humanoid") and v.Humanoid.Health > 0 then
             local mag = (v.HumanoidRootPart.Position - plr.Character.HumanoidRootPart.Position).magnitude
             if mag < dist then
                 closest = v
@@ -27,13 +37,17 @@ local getClosest = function()
     return closest
 end
 
+
 local TpEnemy = function()
     Util.CTween:go(plr, getClosest().HumanoidRootPart.CFrame, 1) 
 end
 
 local Click = function()
-    mouse1click(Mouse.X, Mouse.Y)
+    local screenSize = workspace.CurrentCamera.ViewportSize
+    local screenCenter = Vector2.new(screenSize.X / 2, screenSize.Y / 2)
+    mouse1click(screenCenter.X, screenCenter.Y)
 end
+
 
 local KillEnemy = function()
     local Enemy = getClosest()
@@ -47,8 +61,8 @@ end
 
 local AutoFarm = function()
     while wait() do
-        KillEnemy()
         TpEnemy()
+        KillEnemy()
     end
 end
 
