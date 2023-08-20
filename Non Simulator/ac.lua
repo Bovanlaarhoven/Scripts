9e1903f15035779815b7597fbc104a0275505e29
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-08-20 14:04:27 +0200

    Update ac.lua

--- Non Simulator/ac.lua
+++ Non Simulator/ac.lua
@@ -1,37 +1,50 @@
-local Predix = ";"
-
-return {
-    Kick = function()
-        return Predix .. "kick"
-    end,
-    Ban = function()
-        return Predix .. "ban"
-    end,
-    Unban = function()
-        return Predix .. "unban"
-    end,
-    Shutdown = function()
-        return Predix .. "shutdown"
-    end,
-    Goto = function()
-        return Predix .. "goto"
-    end,
-    Bring = function()
-        return Predix .. "bring"
-    end,
-    Invisible = function()
-        return Predix .. "invisible"
-    end,
-    Visible = function()
-        return Predix .. "visible"
-    end,
-    God = function()
-        return Predix .. "god"
-    end,
-    Ungod = function()
-        return Predix .. "ungod"
-    end,
-    Kill = function()
-        return Predix .. "kill"
-    end,
+local Ranks = {
+    "Admin",
+    "Moderator",
+    "VIP",
+    "Player"
 }
+
+local Userids = {
+    123456789,
+}
+
+local GetRank = function(Userid)
+    for i,v in pairs(Userids) do
+        if v == Userid then
+            return Ranks[i]
+        end
+    end
+    return "Player"
+end
+
+local GetPlayerRank = function(Player)
+    return GetRank(Player.UserId)
+end
+
+local GetPlayerRankColor = function(Player)
+    local Rank = GetPlayerRank(Player)
+    if Rank == "Admin" then
+        return Color3.fromRGB(255, 0, 0)
+    elseif Rank == "Moderator" then
+        return Color3.fromRGB(255, 255, 0)
+    elseif Rank == "VIP" then
+        return Color3.fromRGB(0, 255, 0)
+    elseif Rank == "Player" then
+        return Color3.fromRGB(255, 255, 255)
+    end
+end
+
+local GetPlayerRankTag = function(Player)
+    local Rank = GetPlayerRank(Player)
+    if Rank == "Admin" then
+        return "[Admin]"
+    elseif Rank == "Moderator" then
+        return "[Mod]"
+    elseif Rank == "VIP" then
+        return "[VIP]"
+    elseif Rank == "Player" then
+        return ""
+    end
+end
+
