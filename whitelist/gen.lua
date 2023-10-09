09a9f89c8141c69426e3490298a553881327a2a6
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-10-09 19:57:47 +0200

    oki

--- whitelist/gen.lua
+++
@@ -1,8 +0,0 @@
-local inputString = "my[key1]"
-local byteString = ""
-for i = 1, #inputString do
-    local byte = string.byte(inputString, i)
-    byteString = byteString .. string.format("%02x", byte)
-end
-print(byteString)
-setclipboard(byteString)
